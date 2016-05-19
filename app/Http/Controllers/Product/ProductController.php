b63eb2a34e8c7ff0c0989dbb0285262fdf5a8f14
vijaysebastian <vijaysebastian111@gmail.com>
2016-05-19 03:09:50 -0400

    Applied fixes from StyleCI

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -519,10 +519,12 @@ class ProductController extends Controller
                     ->price;
             }
             $field = $this->getProductField($id);
-            $result = ['price'=>$price,'field'=>$field];
+            $result = ['price' => $price, 'field' => $field];
+
             return response()->json($result);
         } catch (\Exception $ex) {
-            $result = ['price'=>$ex->getMessage()];
+            $result = ['price' => $ex->getMessage()];
+
             return response()->json($result);
         }
     }
@@ -543,13 +545,13 @@ class ProductController extends Controller
             throw new \Exception($ex->getMessage());
         }
     }
-    
-    public function getProductField($productid){
-        try{
-           
+
+    public function getProductField($productid)
+    {
+        try {
             $product = $this->product->find($productid);
-            if($product){
-                if($product->require_domain==1){
+            if ($product) {
+                if ($product->require_domain == 1) {
                     return "<div class='col-md-4 form-group'>
                         <label class='required'>".\Lang::get('message.domain')."</label>
                         <input type='text' name='domain' class='form-control' id='domain' placeholder='http://example.com'>

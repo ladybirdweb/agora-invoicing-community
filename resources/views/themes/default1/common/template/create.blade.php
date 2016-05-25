b10b47bca5494acc78eab433e573f4eb57b9d285
Vijay Sebastian <vijay.sebastian@ladybirdweb.com>
2016-05-25 13:07:14 +0530

    tinymce changed

--- resources/views/themes/default1/common/template/create.blade.php
+++ resources/views/themes/default1/common/template/create.blade.php
@@ -75,19 +75,30 @@
                         <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
   <script>
    tinymce.init({
-  selector: 'textarea',
-  height: 500,
-  plugins: [
-    'advlist autolink lists link image charmap print preview anchor',
-    'searchreplace visualblocks code fullscreen',
-    'insertdatetime media table contextmenu paste code'
-  ],
-  toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
-  content_css: [
-    '//fast.fonts.net/cssapi/e6dc9b99-64fe-4292-ad98-6974f93cd2a2.css',
-    '//www.tinymce.com/css/codepen.min.css'
-  ]
-});
+                          selector: 'textarea',
+                          height: 500,
+                          theme: 'modern',
+                          relative_urls: false,
+                          remove_script_host: false,
+                          convert_urls: true,
+                          plugins: [
+                              'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+                              'searchreplace wordcount visualblocks visualchars code fullscreen',
+                              'insertdatetime media nonbreaking save table contextmenu directionality',
+                              'emoticons template paste textcolor colorpicker textpattern imagetools'
+                          ],
+                          toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+                          toolbar2: 'print preview media | forecolor backcolor emoticons',
+                          image_advtab: true,
+                          templates: [
+                              {title: 'Test template 1', content: 'Test 1'},
+                              {title: 'Test template 2', content: 'Test 2'}
+                          ],
+                          content_css: [
+                              '//fast.fonts.net/cssapi/e6dc9b99-64fe-4292-ad98-6974f93cd2a2.css',
+                              '//www.tinymce.com/css/codepen.min.css'
+                          ]
+                      });
 </script>
                         
                         {!! Form::label('data',Lang::get('message.content'),['class'=>'required']) !!}

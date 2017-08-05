56dc99de57ece8cf8a84bc16adf3f0a304ec572b
Bhanu <bhanu2217@yahoo.com>
2017-08-05 08:40:34 +0000

    Apply fixes from StyleCI

--- public/plugins/dhtmlxSuite_v50_std/samples/dhtmlxCombo/common/config.php
+++ public/plugins/dhtmlxSuite_v50_std/samples/dhtmlxCombo/common/config.php
@@ -1,11 +1,11 @@
 <?php
 
-/* 
+/*
     To run sample application you need to have:
     - PHP
     - MySQL
-    
-    To install application, just make sure that your MySQL running and load index.php 
+
+    To install application, just make sure that your MySQL running and load index.php
     after entering suitable (for you) information below
 */
     $mysql_host = '127.0.0.1';

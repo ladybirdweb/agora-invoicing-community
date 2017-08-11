95cbeda6edcf46d633207f1f437d02128efbe475
lohithkumar1707 <lohithkumar.r@ladybirdweb.com>
2017-08-11 18:37:18 +0530

    updates

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -375,7 +375,6 @@ class AuthController extends Controller
             return response()->json($response);
         } catch (\Exception $ex) {
             $result = [$ex->getMessage()];
-
             return response()->json(compact('result'), 500);
         }
     }

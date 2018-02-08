382d9175c6a14712fc56f6c65f84e7fb2be74c1f
Arindam Jana <arindam.bg91@gmail.com>
2018-02-08 23:38:01 +0530

    update

--- resources/views/themes/default1/front/auth/login-register.blade.php
+++ resources/views/themes/default1/front/auth/login-register.blade.php
@@ -15,7 +15,23 @@ main
 @section('content')
 <?php
 
-$location = json_decode(file_get_contents('http://ip-api.com/json'),true);
+if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+    {
+      $ip=$_SERVER['HTTP_CLIENT_IP'];
+    }
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+    {
+      $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    else
+    {
+      $ip=$_SERVER['REMOTE_ADDR'];
+    }
+
+  
+   $location = json_decode(file_get_contents('http://ip-api.com/json/'.$ip),true);
+
+// $location = json_decode(file_get_contents('http://ip-api.com/json'),true);
 
 $country = \App\Http\Controllers\Front\CartController::findCountryByGeoip($location['countryCode']);
 //$states = \App\Http\Controllers\Front\CartController::findStateByRegionId($location['isoCode']);

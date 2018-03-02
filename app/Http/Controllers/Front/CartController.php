ed722d1af65ca0dcc440cbf0a4ea164e924ca093
Ashutosh pathak <ashutosh.pathak@ladybirdweb.com>
2018-03-02 13:40:24 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Front/CartController.php
+++ app/Http/Controllers/Front/CartController.php
@@ -46,26 +46,24 @@ class CartController extends Controller
 
     public function productList(Request $request)
     {
-        try{
-        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {   //check ip from share internet
-            $ip = $_SERVER['HTTP_CLIENT_IP'];
-        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {   //to check ip is pass from proxy
-            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        } else {
-            $ip = $_SERVER['REMOTE_ADDR'];
-        }
+        try {
+            if (!empty($_SERVER['HTTP_CLIENT_IP'])) {   //check ip from share internet
+                $ip = $_SERVER['HTTP_CLIENT_IP'];
+            } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {   //to check ip is pass from proxy
+                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            } else {
+                $ip = $_SERVER['REMOTE_ADDR'];
+            }
 
-        if ($ip != '::1') {
-            $location = json_decode(file_get_contents('http://ip-api.com/json/'.$ip), true);
-        } else {
-            $location = json_decode(file_get_contents('http://ip-api.com/json'), true);
+            if ($ip != '::1') {
+                $location = json_decode(file_get_contents('http://ip-api.com/json/'.$ip), true);
+            } else {
+                $location = json_decode(file_get_contents('http://ip-api.com/json'), true);
+            }
+        } catch (\Exception $ex) {
+            $location = false;
+            $error = $ex->getMessage();
         }
-    }
-        catch(\Exception $ex){
-             $location = false;
-             $error = $ex->getMessage();
-         }
-            
 
         $country = \App\Http\Controllers\Front\CartController::findCountryByGeoip($location['countryCode']);
         $states = \App\Http\Controllers\Front\CartController::findStateByRegionId($location['countryCode']);
@@ -792,11 +790,10 @@ class CartController extends Controller
                 $subregion = \App\Model\Common\State::where('state_subdivision_code', $code)->first();
                 if ($subregion) {
                     $result = ['id' => $subregion->state_subdivision_code, 'name' => $subregion->state_subdivision_name];
-                    //return ['id' => $subregion->state_subdivision_code, 'name' => $subregion->state_subdivision_name];
+                //return ['id' => $subregion->state_subdivision_code, 'name' => $subregion->state_subdivision_name];
+                } else {
+                    $result = '';
                 }
-                 else {
-                $result = '';
-            }
             }
 
             return $result;

502c72287f484286eaddfe488d1a6d108384955d
Ashutosh pathak <ashutosh.pathak@ladybirdweb.com>
2018-03-08 05:46:50 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Front/PageController.php
+++ app/Http/Controllers/Front/PageController.php
@@ -292,11 +292,11 @@ class PageController extends Controller
         // 'default'                 => false, ];
           if (!empty($_SERVER['HTTP_CLIENT_IP'])) {   //check ip from share internet
           $ip = $_SERVER['HTTP_CLIENT_IP'];
-        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {   //to check ip is pass from proxy
-            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        } else {
-            $ip = $_SERVER['REMOTE_ADDR'];
-        }
+          } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {   //to check ip is pass from proxy
+              $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+          } else {
+              $ip = $_SERVER['REMOTE_ADDR'];
+          }
 
         if ($ip != '::1') {
             $location = json_decode(file_get_contents('http://ip-api.com/json/'.$ip), true);

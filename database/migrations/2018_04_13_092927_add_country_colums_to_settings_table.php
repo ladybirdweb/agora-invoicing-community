56bf6a699fe795e2364a54ee1335f6ffe352b98c
Ashutosh pathak <ashutosh.pathak@ladybirdweb.com>
2018-04-13 10:38:50 +0000

    Apply fixes from StyleCI

--- database/migrations/2018_04_13_092927_add_country_colums_to_settings_table.php
+++ database/migrations/2018_04_13_092927_add_country_colums_to_settings_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class AddCountryColumsToSettingsTable extends Migration
 {
@@ -14,7 +14,7 @@ class AddCountryColumsToSettingsTable extends Migration
     public function up()
     {
         Schema::table('settings', function (Blueprint $table) {
-           $table->string('country');
+            $table->string('country');
         });
     }
 

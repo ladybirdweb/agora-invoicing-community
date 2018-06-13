fcc6796f4ad05702cafe76bb69d20666920732e9
ashu555 <alerts@ladybirdweb.com>
2018-06-14 02:06:56 +0530

    commit

--- config/database.php
+++ config/database.php
@@ -57,6 +57,8 @@ return [
             'engine'      => env('DB_ENGINE', null),
         ],
 
+        
+
         'pgsql' => [
             'driver'   => 'pgsql',
             'host'     => env('DB_HOST', '127.0.0.1'),

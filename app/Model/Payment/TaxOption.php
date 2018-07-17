efe6f5a3fa38c9522c54077ea5d0f160bf734127
Ashutosh pathak <ashutosh.pathak@ladybirdweb.com>
2018-07-17 17:15:28 +0000

    Apply fixes from StyleCI

--- app/Model/Payment/TaxOption.php
+++ app/Model/Payment/TaxOption.php
@@ -7,15 +7,14 @@ use Spatie\Activitylog\Traits\LogsActivity;
 
 class TaxOption extends BaseModel
 {
-	use LogsActivity;
+    use LogsActivity;
     protected $table = 'tax_rules';
     protected static $logName = 'Tax Class';
     protected static $logAttributes = ['tax_enable', 'inclusive', 'shop_inclusive', 'cart_inclusive', 'rounding', 'Gst_no'];
-     protected static $logOnlyDirty = true;
+    protected static $logOnlyDirty = true;
     protected $fillable = ['tax_enable', 'inclusive', 'shop_inclusive', 'cart_inclusive', 'rounding', 'Gst_no'];
 
-  
-   public function getDescriptionForEvent(string $eventName): string
+    public function getDescriptionForEvent(string $eventName): string
     {
         if ($eventName == 'created') {
             return 'Tax Rule  <strong> '.$this->name.' </strong> was created';

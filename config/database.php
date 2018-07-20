18fefb2e219a0d8b893dca8c81961c37c6f1e14a
ashu555 <alerts@ladybirdweb.com>
2018-07-20 12:24:44 +0530

    update

--- config/database.php
+++ config/database.php
@@ -40,24 +40,24 @@ return [
             'prefix'   => '',
         ],
 
-        // 'mysql' => [
-        //     'driver'      => 'mysql',
-        //     'host'        => env('DB_HOST', 'localhost'),
-        //     'port'        => env('DB_PORT', ''),
-        //     'database'    => env('DB_DATABASE', ''),
-
-        //     'username'    => env('DB_USERNAME', 'root'),
-        //     'password'    => env('DB_PASSWORD', ''),
-
-        //     'unix_socket' => env('DB_SOCKET', ''),
-        //     'charset'     => 'utf8mb4',
-        //     'collation'   => 'utf8mb4_unicode_ci',
-        //     'prefix'      => '',
-        //     'strict'      => false,
-        //     'engine'      => env('DB_ENGINE', null),
-        // ],
-
         'mysql' => [
+            'driver'      => 'mysql',
+            'host'        => env('DB_HOST', 'localhost'),
+            'port'        => env('DB_PORT', ''),
+            'database'    => env('DB_DATABASE', ''),
+
+            'username'    => env('DB_USERNAME', 'root'),
+            'password'    => env('DB_PASSWORD', ''),
+
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset'     => 'utf8mb4',
+            'collation'   => 'utf8mb4_unicode_ci',
+            'prefix'      => '',
+            'strict'      => false,
+            'engine'      => env('DB_ENGINE', null),
+        ],
+
+        'testing' => [
         'driver'    => 'mysql',
         'host'      => env('DB_TEST_HOST', 'localhost'),
         'database'  => env('DB_TEST_DATABASE', 'db_testing'),

5ef34a901f829a4e964ab3b935928e2aaf07469f
ashu555 <alerts@ladybirdweb.com>
2018-07-28 16:52:26 +0530

    travis

--- config/database.php
+++ config/database.php
@@ -57,17 +57,17 @@ return [
             'engine'      => env('DB_ENGINE', null),
         ],
 
-      // 'testing' => [
-      //       'driver'    => 'mysql',
-      //       'host'      => env('DB_TEST_HOST', 'localhost'),
-      //       'database'  => env('DB_TEST_DATABASE', 'db_testing'),
-      //       'username'  => env('DB_TEST_USERNAME', 'root'),
-      //       'password'  => env('DB_TEST_PASSWORD', ''),
-      //       'charset'   => 'utf8',
-      //       'collation' => 'utf8_unicode_ci',
-      //       'prefix'    => '',
-      //       'strict'    => false,
-      //   ],
+      'testing' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'db_testing'),
+            'username'  => env('DB_USERNAME', 'root'),
+            'password'  => env('DB_PASSWORD', ''),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
 
         'pgsql' => [
             'driver'   => 'pgsql',

d8edd55b8df4a6100bbfd70f30f07e0d70fd7e84
Ashutosh pathak <ashutosh.pathak@ladybirdweb.com>
2018-09-03 13:55:38 +0000

    Apply fixes from StyleCI

--- database/migrations/2018_09_03_115525_add_status_to_currencies.php
+++ database/migrations/2018_09_03_115525_add_status_to_currencies.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class AddStatusToCurrencies extends Migration
 {
@@ -13,9 +13,8 @@ class AddStatusToCurrencies extends Migration
      */
     public function up()
     {
-         Schema::table('currencies', function (Blueprint $table) {
+        Schema::table('currencies', function (Blueprint $table) {
             $table->boolean('active')->nullable();
-          
         });
     }
 

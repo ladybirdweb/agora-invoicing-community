5e7f235e85a601638522f986b8d07c4154ad46af
ashu555 <alerts@ladybirdweb.com>
2018-10-16 16:16:21 +0530

    Update database/factories/UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,7 +2,7 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(\App\User::class, function (Faker $faker) {
     return [
        'user_name'       => $faker->userName,
        'first_name'      => $faker->firstName,

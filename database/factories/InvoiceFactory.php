42b692d6ad50c5a6790fc7e6a776fb2c15c9b5e4
ashu555 <alerts@ladybirdweb.com>
2018-10-16 15:45:30 +0530

    update

--- database/factories/InvoiceFactory.php
+++ database/factories/InvoiceFactory.php
@@ -4,7 +4,7 @@ use App\Model\Order\Invoice;
 use Faker\Generator as Faker;
 
 $factory->define(Invoice::class, function (Faker $faker) {
-    return [
+   return [
          'number'     => '2344353',
         'grand_total' => 10000,
         'currency'    => 'INR',

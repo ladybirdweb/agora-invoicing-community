f1c5211c74c0606d84f137372c698973b4e07b4a
Ashutosh pathak <ashutosh.pathak@ladybirdweb.com>
2018-11-02 08:01:04 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/License/LicenseController.php
+++ app/Http/Controllers/License/LicenseController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\License;
 
 use App\Http\Controllers\Controller;
-use App\Model\Product\Product;
 use App\Model\Order\Order;
+use App\Model\Product\Product;
 use App\User;
 
 class LicenseController extends Controller
@@ -70,18 +70,17 @@ class LicenseController extends Controller
             $getProductId = $this->postCurl($url, "api_key_secret=$api_key_secret&api_function=search
       &search_type=product&search_keyword=$product_sku");
 
-      $details =  json_decode($getProductId);
-      if($details->api_error_detected == 0 && is_array($details->page_message)) {//This is not true if Product_sku is updated
-        $productId = $details->page_message[0]->product_id;
-       }
-      
-      return $productId;
-      }catch (\Exception $ex) {
-         $result = [$ex->getMessage()];
-         return response()->json(compact('result'), 500);
-      }
-      
+            $details = json_decode($getProductId);
+            if ($details->api_error_detected == 0 && is_array($details->page_message)) {//This is not true if Product_sku is updated
+                $productId = $details->page_message[0]->product_id;
+            }
 
+            return $productId;
+        } catch (\Exception $ex) {
+            $result = [$ex->getMessage()];
+
+            return response()->json(compact('result'), 500);
+        }
     }
 
     /*
@@ -107,34 +106,30 @@ class LicenseController extends Controller
         $getUserId = $this->postCurl($url, "api_key_secret=$api_key_secret&api_function=search
       &search_type=client&search_keyword=$email");
 
-      $details =  json_decode($getUserId);
-    if($details->api_error_detected == 0 && is_array($details->page_message)) {//This is not true if email is updated
-       $userId = $details->page_message[0]->client_id;
-    }
-      return $userId;
-    }
-    
+        $details = json_decode($getUserId);
+        if ($details->api_error_detected == 0 && is_array($details->page_message)) {//This is not true if email is updated
+            $userId = $details->page_message[0]->client_id;
+        }
 
-   /*
-   *  Create New License For User 
-   */
-   public function createNewLicene($orderid,$product,$user_id,$ends_at )
-   {
-     $url = "https://license.faveohelpdesk.com/apl_api/api.php";
-      $expiry = $ends_at->toDateString();
-      $api_key_secret = "0bs8ArC9Tp1mG6Cg";
-      $sku = Product::where('id',$product)->first()->product_sku;
-      $order = Order::where('id',$orderid)->first();
-      $orderNo = $order->number;
-      $domain = $order->domain;
-      $email = User::where('id',$user_id)->first()->email;
-      $userId = $this->searchForUserId($email);
-      $productId = $this->searchProductId($sku);
-       $addLicense= $this->postCurl($url, "api_key_secret=$api_key_secret&api_function=licenses_add&product_id=$productId&client_id=$userId
+        return $userId;
+    }
+
+    /*
+    *  Create New License For User
+    */
+    public function createNewLicene($orderid, $product, $user_id, $ends_at)
+    {
+        $url = 'https://license.faveohelpdesk.com/apl_api/api.php';
+        $expiry = $ends_at->toDateString();
+        $api_key_secret = '0bs8ArC9Tp1mG6Cg';
+        $sku = Product::where('id', $product)->first()->product_sku;
+        $order = Order::where('id', $orderid)->first();
+        $orderNo = $order->number;
+        $domain = $order->domain;
+        $email = User::where('id', $user_id)->first()->email;
+        $userId = $this->searchForUserId($email);
+        $productId = $this->searchProductId($sku);
+        $addLicense = $this->postCurl($url, "api_key_secret=$api_key_secret&api_function=licenses_add&product_id=$productId&client_id=$userId
       &license_require_domain=1&license_status=1&license_order_number=$orderNo&license_domain=$domain&license_limit=5&license_expire_date=$expiry&license_disable_ip_verification=0");
-
-   }
-
-
-    
+    }
 }

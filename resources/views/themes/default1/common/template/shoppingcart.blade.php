527b93c410870f8f18d996f46d5023d2409e8783
ashu555 <alerts@ladybirdweb.com>
2018-11-26 16:33:09 +0530

    update

--- resources/views/themes/default1/common/template/shoppingcart.blade.php
+++ resources/views/themes/default1/common/template/shoppingcart.blade.php
@@ -66,77 +66,18 @@ main
                             <h4 style="font-size: 30px;text-align: center;font-weight: bold;margin-left:400px">Helpdesk Self Hosted Pricing</h4>
                         
                     </div>
-    <div class="col-md-12">
-        <div class="pricing-table princig-table-flat">
-            <div class="row">
+   
         {!! html_entity_decode($template) !!}
-    </div>
-        </div>
-    </div>
+ 
     <br/>    <br/>    <br/>    <br/>  <br/> <br/>
-           <div class="row">
-                        
-            
-            <h4 style="font-size: 30px;text-align: center;font-weight: bold;margin-left:400px;margin-top: 65px">Helpdesk VPS Hosting Pricing</h4>
-                        
-                    </div>
-    <div class="col-md-12">
-        <div class="pricing-table princig-table-flat">
-            <div class="row">
-        {!! html_entity_decode($helpdesk_vps_template) !!}
-    </div>
-        </div>
-    </div>
 
 
 
-                    <div class="row">
-                       
-                            <!-- <hr class="tall mt-none"> -->
-                            <h4 style="font-size: 30px;text-align: center;font-weight: bold;margin-top: 65px;margin-left:400px"><strong><center>Service Desk Self Hosted Pricing
 
 
-</center></strong></h4>
-                      
-                    </div>
-  <div class="col-md-12">
-   <div class="pricing-table princig-table-flat">
-     <div class="row">
-        {!! html_entity_decode($servicedesk_template) !!}
-        </div>
-    </div>
-
-</div>
  <br/>    <br/>    <br/>    <br/>  <br/> <br/>
 
-            <div class="row">
-                        
-            
-            <h4 style="font-size: 30px;text-align: center;font-weight: bold;margin-left:400px;margin-top: 65px">ServiceDesk VPS Hosting Pricing</h4>
-                        
-                    </div>
-    <div class="col-md-12">
-        <div class="pricing-table princig-table-flat">
-            <div class="row">
-        {!! html_entity_decode($servicedesk_vps_template) !!}
-    </div>
-        </div>
-    </div>
 
-  <div class="row">
-                       
-                           <!--  <hr class="tall mt-none"> -->
-                            <h4 style="font-size: 30px;text-align: center;font-weight: bold;margin-top:65px;margin-left:450px;"><strong><center>Services Pricing</center></strong></h4>
-                      
-                    </div>
-<div class="col-md-12" style="margin-bottom:40px;">
-   <div class="pricing-table princig-table-flat">
-    <div class= "row">
-        {!! html_entity_decode($service_template) !!}
-    </div>
-        </div>
-
-</div>
 
 
 
--- resources/views/themes/default1/front/page/create.blade.php
+++ resources/views/themes/default1/front/page/create.blade.php
@@ -107,12 +107,31 @@ Create New Page
 
                         <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
                         <script>
-    tinymce.init({
-    selector: 'textarea',
-    plugins: "code",
-    toolbar: "code",
-    menubar: "tools"
-});
+             tinymce.init({
+                          selector: 'textarea',
+                          height: 500,
+                          theme: 'modern',
+                          relative_urls: true,
+                          remove_script_host: false,
+                          convert_urls: false,
+                          plugins: [
+                              'advlist autolink lists link image charmap print preview hr anchor pagebreak',
+                              'searchreplace wordcount visualblocks visualchars code fullscreen',
+                              'insertdatetime media nonbreaking save table contextmenu directionality',
+                              'emoticons template paste textcolor colorpicker textpattern imagetools'
+                          ],
+                          toolbar1: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+                          toolbar2: 'print preview media | forecolor backcolor emoticons',
+                          image_advtab: true,
+                          templates: [
+                              {title: 'Test template 1', content: 'Test 1'},
+                              {title: 'Test template 2', content: 'Test 2'}
+                          ],
+                          content_css: [
+                              '//fast.fonts.net/cssapi/e6dc9b99-64fe-4292-ad98-6974f93cd2a2.css',
+                              '//www.tinymce.com/css/codepen.min.css'
+                          ]
+                      });
 </script>
 
 

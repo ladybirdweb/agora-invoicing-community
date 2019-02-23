e89884532f93d03f3f270cd4d3089a68f0ee3297
Ashutosh pathak <ashutosh.pathak@ladybirdweb.com>
2019-02-23 07:12:51 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Common/BaseMailChimpController.php
+++ app/Http/Controllers/Common/BaseMailChimpController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Common;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Model\Common\StatusSetting;
+use Illuminate\Http\Request;
 
 class BaseMailChimpController extends Controller
 {

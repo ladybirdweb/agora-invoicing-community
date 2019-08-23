f74494658a181a840a7d349c4858e0939f0844b6
Ashutosh pathak <ashutosh.pathak@ladybirdweb.com>
2019-08-23 11:28:36 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Common/SystemManagerController.php
+++ app/Http/Controllers/Common/SystemManagerController.php
@@ -2,16 +2,16 @@
 
 namespace App\Http\Controllers\Common;
 
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Controller;
 use App\User;
+use Illuminate\Http\Request;
 
 class SystemManagerController extends Controller
 {
     public function getSystemManagers()
     {
-    	$accountManagers = User::where('role', 'admin')
+        $accountManagers = User::where('role', 'admin')
             ->where('position', 'account_manager')
             ->pluck('first_name', 'id')->toArray();
 
@@ -19,7 +19,7 @@ class SystemManagerController extends Controller
         ->where('position', 'manager')
         ->pluck('first_name', 'id')->toArray();
 
-        return view('themes.default1.common.system-managers',compact('accountManagers','salesManager'));
+        return view('themes.default1.common.system-managers', compact('accountManagers', 'salesManager'));
     }
 
     public function searchAdmin(Request $request)
@@ -32,15 +32,14 @@ class SystemManagerController extends Controller
             $users = User::where('email', 'LIKE', '%'.$term.'%')
              ->orWhere('first_name', 'LIKE', '%'.$term.'%')
              ->orWhere('last_name', 'LIKE', '%'.$term.'%')
-             ->select('id', 'email', 'profile_pic', 'first_name', 'last_name','role')->get();
+             ->select('id', 'email', 'profile_pic', 'first_name', 'last_name', 'role')->get();
             $formatted_tags = [];
 
             foreach ($users as $user) {
-                if($user->role == 'admin') {
+                if ($user->role == 'admin') {
                     $formatted_users[] = ['id'     => $user->id, 'text' => $user->email, 'profile_pic' => $user->profile_pic,
-                'first_name'                   => $user->first_name, 'last_name' => $user->last_name, ];
+                'first_name'                       => $user->first_name, 'last_name' => $user->last_name, ];
                 }
-                
             }
 
             return \Response::json($formatted_users);
@@ -51,96 +50,95 @@ class SystemManagerController extends Controller
     }
 
     /**
-     * Replace old account manager with the newly selected account manager
+     * Replace old account manager with the newly selected account manager.
      *
      * @author Ashutosh Pathak <ashutosh.pathak@ladybirdweb.com>
      *
      * @date   2019-08-21T12:54:03+0530
      *
-     * @param  Request $request
+     * @param Request $request
      *
      * @return array
      */
     public function replaceAccountManager(Request $request)
     {
-         $this->validate($request,[
+        $this->validate($request, [
             'existingAccManager' => 'required',
-            'newAccManager'     => 'required',
-        ],[
+            'newAccManager'      => 'required',
+        ], [
             'existingAccManager.required' => 'Select system Account Manager',
-            'newAccManager.required'    => 'Select new Account Manager'
+            'newAccManager.required'      => 'Select new Account Manager',
         ]);
-        try {
 
-        $existingAccManager = $request->input('existingAccManager');
-        $newAccountManager = $request->input('newAccManager')[0];
-        if($existingAccManager == $newAccountManager) {
-             return ['message'=>'fails', 'update'=>'Existing and the new account manager cannot be same'];
-        }
-        //First make the selected Admin as account Manager-
-        User::where('id',$newAccountManager)->update(['position'=>'account_manager']);
-        $accManagers = User::where('account_manager',$existingAccManager)->get();
-         foreach ($accManagers as $accManager) {
-            User::where('id',$accManager->id)->update(['account_manager'=>$newAccountManager]);
-        }
-         $arrayOfBccEmails = User::where('account_manager',$newAccountManager)->pluck('email')->toArray();
-         if(count($arrayOfBccEmails) > 0) {
-            $user = User::where('email',$arrayOfBccEmails[0])->first();
-            $cont = new AuthController();
-            $sendMail = $cont->accountManagerMail($user,$arrayOfBccEmails);
-         }
-        
-        return ['message' => 'success', 'update'=>\Lang::get('message.account_man_replaced_success')];
+        try {
+            $existingAccManager = $request->input('existingAccManager');
+            $newAccountManager = $request->input('newAccManager')[0];
+            if ($existingAccManager == $newAccountManager) {
+                return ['message'=>'fails', 'update'=>'Existing and the new account manager cannot be same'];
+            }
+            //First make the selected Admin as account Manager-
+            User::where('id', $newAccountManager)->update(['position'=>'account_manager']);
+            $accManagers = User::where('account_manager', $existingAccManager)->get();
+            foreach ($accManagers as $accManager) {
+                User::where('id', $accManager->id)->update(['account_manager'=>$newAccountManager]);
+            }
+            $arrayOfBccEmails = User::where('account_manager', $newAccountManager)->pluck('email')->toArray();
+            if (count($arrayOfBccEmails) > 0) {
+                $user = User::where('email', $arrayOfBccEmails[0])->first();
+                $cont = new AuthController();
+                $sendMail = $cont->accountManagerMail($user, $arrayOfBccEmails);
+            }
+
+            return ['message' => 'success', 'update'=>\Lang::get('message.account_man_replaced_success')];
         } catch (\Exception $ex) {
             return ['message'=>'fails', 'update'=>$ex->getMessage()];
         }
-
     }
 
-        /**
-     * Replace old sales manager with the newly selected sales manager
+    /**
+     * Replace old sales manager with the newly selected sales manager.
      *
      * @author Ashutosh Pathak <ashutosh.pathak@ladybirdweb.com>
      *
      * @date   2019-08-21T12:54:03+0530
      *
-     * @param  Request $request
+     * @param Request $request
      *
      * @return array
      */
     public function replaceSalesManager(Request $request)
     {
-         $this->validate($request,[
+        $this->validate($request, [
             'existingSaleManager' => 'required',
-            'newSaleManager'     => 'required',
-        ],[
+            'newSaleManager'      => 'required',
+        ], [
             'existingSaleManager.required' => 'Select system Sales Manager',
-            'newSaleManager.required'    => 'Select new Sales Manager'
+            'newSaleManager.required'      => 'Select new Sales Manager',
         ]);
-        try {
-        $existingSaleManager = $request->input('existingSaleManager');
-        $newSalesManager = $request->input('newSaleManager')[0];
-        if($existingSaleManager == $newSalesManager) {
-             return ['message'=>'fails', 'update'=>'Existing and the new sales manager cannot be same'];
-        }
-        //First make the selected Admin as account Manager-
-        User::where('id',$newSalesManager)->update(['position'=>'manager']);
 
-        $saleManagers = User::where('manager',$existingSaleManager)->get();
-        foreach ($saleManagers as $saleManager) {
-            User::where('id',$saleManager->id)->update(['manager'=>$newSalesManager]);
-        }
-        $arrayOfBccEmails = User::where('manager',$newSalesManager)->pluck('email')->toArray();
-         if(count($arrayOfBccEmails) > 0) {
-            $user = User::where('email',$arrayOfBccEmails[0])->first();
-            $cont = new AuthController();
-            $sendMail = $cont->salesManagerMail($user,$arrayOfBccEmails);
-         }
-       
-        return ['message' => 'success', 'update'=>\Lang::get('message.sales_man_replaced_success')];
+        try {
+            $existingSaleManager = $request->input('existingSaleManager');
+            $newSalesManager = $request->input('newSaleManager')[0];
+            if ($existingSaleManager == $newSalesManager) {
+                return ['message'=>'fails', 'update'=>'Existing and the new sales manager cannot be same'];
+            }
+            //First make the selected Admin as account Manager-
+            User::where('id', $newSalesManager)->update(['position'=>'manager']);
+
+            $saleManagers = User::where('manager', $existingSaleManager)->get();
+            foreach ($saleManagers as $saleManager) {
+                User::where('id', $saleManager->id)->update(['manager'=>$newSalesManager]);
+            }
+            $arrayOfBccEmails = User::where('manager', $newSalesManager)->pluck('email')->toArray();
+            if (count($arrayOfBccEmails) > 0) {
+                $user = User::where('email', $arrayOfBccEmails[0])->first();
+                $cont = new AuthController();
+                $sendMail = $cont->salesManagerMail($user, $arrayOfBccEmails);
+            }
+
+            return ['message' => 'success', 'update'=>\Lang::get('message.sales_man_replaced_success')];
         } catch (\Exception $ex) {
             return ['message'=>'fails', 'update'=>$ex->getMessage()];
         }
-
     }
 }

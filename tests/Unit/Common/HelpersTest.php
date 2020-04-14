2a98d7601abd6b9bbb937b42e0a00fd983b65e15
avinash kumar <alerts@ladybirdweb.com>
2020-04-14 20:30:09 +0530

    #990: timezone common implementation with caching

---
+++ tests/Unit/Common/HelpersTest.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace Tests\Unit\Common;
+
+
+use Carbon\Carbon;
+use Tests\DBTestCase;
+use Cache;
+
+class HelpersTest extends DBTestCase
+{
+    public function test_getTimeInLoggedInUserTimeZone_whenUserTimezoneIsNull_shouldConsiderTimezoneAsUTC()
+    {
+        $this->getLoggedInUser("admin");
+        $this->user->timezone_id = null;
+        $this->assertEquals("Jan 1, 2001, 12:00 am", getTimeInLoggedInUserTimeZone(Carbon::now()->startOfMillennium()));
+    }
+
+    public function test_getTimeInLoggedInUserTimeZone_whenUserTimezoneIsPresent_shouldConsiderTimezoneAsUserTimezone()
+    {
+        $this->getLoggedInUser("admin");
+        $this->user->timezone()->updateOrCreate(["name"=>"Asia/Kolkata"]);
+        $this->assertEquals("Jan 1, 2001, 5:30 am", getTimeInLoggedInUserTimeZone(Carbon::now()->startOfMillennium()));
+    }
+
+    public function test_getTimeInLoggedInUserTimeZone_cachesUserTimezoneForFiveSeconds()
+    {
+        $this->getLoggedInUser("admin");
+
+        Cache::shouldReceive('remember')->once()->withArgs(["timezone_".$this->user->id, 5, \Closure::class])->andReturn("Asia/Kolkata");
+
+        getTimeInLoggedInUserTimeZone(Carbon::now()->startOfMillennium());
+    }
+}

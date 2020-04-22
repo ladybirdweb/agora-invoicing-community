9caf683405119f248cb3c440a3e9284dfade7af0
Phaniraj35 <phaniitng@gmail.com>
2020-04-22 17:52:18 +0530

    Seeder Added

--- database/seeds/PlanPriceSeeder.php
+++ database/seeds/PlanPriceSeeder.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Model\Common\Country;
+use App\Model\Common\Setting;
+use App\Model\Payment\Currency;
+use App\Model\Payment\PlanPrice;
 use Illuminate\Database\Seeder;
 
 class PlanPriceSeeder extends Seeder
@@ -11,6 +15,21 @@ class PlanPriceSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $this->mapCountriesToCurrency();
     }
+
+    private function mapCountriesToCurrency()
+    {
+        $nonDefaultCurrencies = PlanPrice::where(
+            'currency', "!=", (new Setting())->first()->default_currency
+        )->get(['id', 'currency']);
+        if ($nonDefaultCurrencies) {
+            foreach ($nonDefaultCurrencies as $currency) {
+                $currencyId = Currency::where('code',$currency->currency)->first()->id;
+                $countryId = Country::where('currency_id',$currencyId)->first()->country_id;
+                $currency->update(['country_id'=>$countryId]);
+            }
+        }
+    }
+
 }

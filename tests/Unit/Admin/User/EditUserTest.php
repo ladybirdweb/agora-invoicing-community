b295e14dc6cf81738fbd031831298c82791b8d8a
ashu555 <ashutosh.pathak@ladybirdweb.com>
2020-05-06 13:06:34 +0530

    test case fix

--- tests/Unit/Admin/User/EditUserTest.php
+++ tests/Unit/Admin/User/EditUserTest.php
@@ -15,8 +15,8 @@ class EditUserTest extends TestCase
     {
         $user = factory(User::class)->create();
         $this->withoutMiddleware();
-        $response = $this->call('PATCH', 'clients/'.$user->id, ['first_name'=> $user->first_name, 'last_name'=> $user->last_name, 'email'=>$user->email, 'user_name'=>$user->user_name, 'company'=>$user->company, 'bussiness'=>$user->bussiness, 'active'=>$user->active, 'mobile_verified'=>$user->mobile_verified, 'role'=>$user->role, 'position'=>'', 'company_type'=>$user->company_type, 'company_size'=>$user->company_size, 'address'=>$user->address, 'town'=>$user->town, 'country'=>$user->country, 'state'=>$user->state, 'zip'=>$user->zip, 'timezone_id'=>'79', 'currency'=>'USD', 'mobile_code'=>'91', 'mobile'=>$user->mobile, 'skype'=>'',
-            'manager'                                                       => 'Ashutosh', ]);
+        $response = $this->call('PATCH', 'clients/'.$user->id, ['first_name'=> $user->first_name, 'last_name'=> $user->last_name, 'email'=>$user->email, 'user_name'=>$user->user_name, 'company'=>$user->company, 'bussiness'=>$user->bussiness, 'active'=>$user->active, 'mobile_verified'=>$user->mobile_verified, 'role'=>$user->role, 'position'=>'', 'company_type'=>$user->company_type, 'company_size'=>$user->company_size, 'address'=>$user->address, 'town'=>$user->town, 'country'=>'IN', 'state'=>$user->state, 'zip'=>$user->zip, 'timezone_id'=>'79', 'currency'=>'USD', 'mobile_code'=>'91', 'mobile'=>$user->mobile, 'skype'=>'',
+            'manager'=> 'Ashutosh','timezone_id'=>$user->timezone_id ]);
         $response->assertSessionHas('success');
     }
 }

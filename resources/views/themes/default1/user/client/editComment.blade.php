0d4739a255264b4dcb2ab0fdb8e2bb23d432415a
ashu555 <ashutosh.pathak@ladybirdweb.com>
2020-06-14 22:30:16 +0530

    Admin Lte update 2

--- resources/views/themes/default1/user/client/editComment.blade.php
+++ resources/views/themes/default1/user/client/editComment.blade.php
@@ -2,8 +2,9 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
+                 <h4 class="modal-title">{{Lang::get('message.edit-comment')}}</h4>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">{{Lang::get('message.edit-comment')}}</h4>
+               
             </div>
            {!! Form::open(['method' => 'patch', 'id' =>  'comment-edit-form'])!!}
             <div class="modal-body">
@@ -15,9 +16,9 @@
                     <input type="hidden" name="updated_by_user_id" id="admin-id" > 
                 </div>
             </div>
-            <div class="modal-footer">
-                 <button type="button" id="close" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
-                <button type="submit" class="btn btn-primary " id="submit" data-loading-text="<i class='fa fa-circle-o-notch fa-spin'>&nbsp;</i> Saving..."><i class="fa fa-refresh">&nbsp;&nbsp;</i>{!!Lang::get('message.update')!!}</button>
+            <div class="modal-footer justify-content-between">
+                 <button type="button" id="close" class="btn btn-default" data-dismiss="modal"><i class="fas fa-times">&nbsp;</i>Close</button>
+                <button type="submit" class="btn btn-primary " id="submit" data-loading-text="<i class='fa fa-circle-o-notch fa-spin'>&nbsp;</i> Saving..."><i class="fas fa-refresh">&nbsp;&nbsp;</i>{!!Lang::get('message.update')!!}</button>
             </div>
             {!! Form::close() !!}
         </div>

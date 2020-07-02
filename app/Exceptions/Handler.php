7ca908b843f72394cb9b01361cd1e412051ae14a
Ashutosh pathak <ashutosh.pathak@ladybirdweb.com>
2020-07-02 12:38:51 +0000

    Apply fixes from StyleCI

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,10 +4,10 @@ namespace App\Exceptions;
 
 use Bugsnag;
 use Exception;
-use Throwable;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Log;
+use Throwable;
 
 class Handler extends ExceptionHandler
 {

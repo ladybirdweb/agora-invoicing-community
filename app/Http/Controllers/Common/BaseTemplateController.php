0d21624459e68d087d6e239542590550834aebfc
Ashutosh pathak <ashu555@users.noreply.github.com>
2020-08-08 06:20:40 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Common/BaseTemplateController.php
+++ app/Http/Controllers/Common/BaseTemplateController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Common;
 use App\Model\Common\Setting;
 use App\Model\Payment\Currency;
 use App\Model\Payment\Plan;
-use App\Model\Common\Setting;
 use Bugsnag;
 
 class BaseTemplateController extends ExtendedBaseTemplateController

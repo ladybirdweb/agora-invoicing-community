7ae299c5d2bff930077338e9e27645f8862ede23
Ashutosh pathak <ashutosh.pathak@ladybirdweb.com>
2020-09-03 16:17:08 +0530

    Queue implementation

--- app/Http/Controllers/Jobs/QueueController.php
+++ app/Http/Controllers/Jobs/QueueController.php
@@ -2,87 +2,86 @@
 
 namespace App\Http\Controllers\Jobs;
 
-use App\Http\Controllers\Common\PHPController as Controller;
-use App\Http\Requests\Queue\QueueRequest;
-use App\Model\Mailjob\FaveoQueue;
-use App\Model\MailJob\QueueService;
+
 use Form;
 use Illuminate\Http\Request;
+use App\Model\MailJob\QueueService;
+use App\Model\Mailjob\FaveoQueue;
+use App\Http\Requests\Queue\QueueRequest;
+use App\Http\Controllers\Common\PHPController as Controller;
 
 class QueueController extends Controller
 {
-    private $queue;
+	private $queue;
 
-    public function __construct()
-    {
-        $this->middleware('auth');
-        $this->middleware('admin');
+	public function __construct()
+{
+    $this->middleware('auth');
+    $this->middleware('admin');
 
-        $this->queue = new QueueService();
+    $this->queue = new QueueService();
+
+}
+
+public function index() {
+    try {
+        $cronPath = base_path('artisan');
+        $queue = new QueueService();
+        $activeQueue = $queue->where('status',1)->first();
+        $paths = $this->getPHPBinPath();
+        return view('themes.default1.queue.index', compact('activeQueue', 'paths', 'cronPath'));
+    } catch (Exception $ex) {
+        return redirect()->back()->with('fails', $ex->getMessage());
     }
+}
 
-    public function index()
-    {
-        try {
-            $cronPath = base_path('artisan');
-            $queue = new QueueService();
-            $activeQueue = $queue->where('status', 1)->first();
-            $paths = $this->getPHPBinPath();
-
-            return view('themes.default1.queue.index', compact('activeQueue', 'paths', 'cronPath'));
-        } catch (Exception $ex) {
-            return redirect()->back()->with('fails', $ex->getMessage());
-        }
-    }
-
-    public function getQueues()
-    {
-        try {
-            $allQueues = $this->queue->select('id', 'name', 'status')->get();
-
-            return \DataTables::of($allQueues)
+public function getQueues()
+{
+	try {
+        $allQueues = $this->queue->select('id', 'name', 'status')->get();
+       
+        return \DataTables::of($allQueues)
         ->addColumn('name', function ($model) {
-            return $model->getName();
+        	return $model->getName();
         })
         ->addColumn('status', function ($model) {
-            return $model->getStatus();
+           return $model->getStatus();
         })
         ->addColumn('action', function ($model) {
-            return $model->getAction();
-        })
+        	return $model->getAction();
+         })
         ->rawColumns(['checkbox', 'name', 'status', 'action'])
         ->make(true);
-        } catch (\Exception $ex) {
-            Bugsnag::notifyException($ex);
-            app('log')->error($ex->getMessage());
+    } catch (\Exception $ex) {
+        Bugsnag::notifyException($ex);
+        app('log')->error($ex->getMessage());
 
-            return redirect()->back()->with('fails', $ex->getMessage());
-        }
+        return redirect()->back()->with('fails', $ex->getMessage());
     }
+}
 
-    public function edit($id)
-    {
-        try {
-            $queues = new QueueService();
-            $queue = $queues->find($id);
-            if (! $queue) {
-                throw new Exception('Sorry we can not find your request');
-            }
-
-            return view('themes.default1.queue.edit', compact('queue'));
-        } catch (Exception $ex) {
-            return redirect()->back()->with('fails', $ex->getMessage());
-        }
+public function edit($id) {
+try {
+    $queues = new QueueService();
+    $queue = $queues->find($id);
+    if (!$queue) {
+        throw new Exception("Sorry we can not find your request");
     }
+    return view('themes.default1.queue.edit', compact('queue'));
+} catch (Exception $ex) {
+    return redirect()->back()->with('fails', $ex->getMessage());
+}
+}
 
-    public function update($id, QueueRequest $request)
-    {
+public function update($id, QueueRequest $request) {
+        
         try {
             $values = $request->except('_token');
             $queues = new QueueService();
             $queue = $queues->find($id);
-            if (! $queue) {
-                throw new Exception('Sorry we can not find your request');
+
+            if (!$queue) {
+                throw new Exception("Sorry we can not find your request");
             }
             $setting = new FaveoQueue();
             $settings = $setting->where('service_id', $id)->get();
@@ -107,52 +106,48 @@ class QueueController extends Controller
         }
     }
 
-    public function getForm(Request $request)
-    {
-        $queueid = $request->input('queueid');
-        $form = $this->getFormById($queueid);
 
-        return $form;
+public function getForm(Request $request){
+    $queueid = $request->input('queueid');
+    $form = $this->getFormById($queueid);
+    return $form;
+}
+
+
+public function activate(Request $request, QueueService $queue) {
+	try {
+	$activeQueue = QueueService::where('status', 1)->first();
+
+	if ($queue->isActivate()==false&&$queue->id!=1&&$queue->id!=2) {
+		return redirect()->back()->with('fails', "To activate $queue->name , Please configure it first");
+	}
+	if ($activeQueue) {
+	    $activeQueue->status = 0;
+	    $activeQueue->save();
+	}
+	$queue->status = 1;
+	$queue->save();
+	// $this->updateSnapShotJob($queue);
+	$result=$queue->name." ". 'Activated successfully';  
+
+	return redirect()->back()->with('success', $result);
+	} catch (Exception $ex) {
+	return redirect()->back()->with('fails', $ex->getMessage());
+	}
+}
+
+public function getShortNameById($queueid) {
+    $short = "";
+    $queues = new QueueService();
+    $queue = $queues->find($queueid);
+    if ($queue) {
+        $short = $queue->short_name;
     }
+    return $short;
+}
 
-    public function activate(Request $request, QueueService $queue)
-    {
-        try {
-            $activeQueue = QueueService::where('status', 1)->first();
-
-            if ($queue->isActivate() == false && $queue->id != 1 && $queue->id != 2) {
-                return redirect()->back()->with('fails', "To activate $queue->name , Please configure it first");
-            }
-            if ($activeQueue) {
-                $activeQueue->status = 0;
-                $activeQueue->save();
-            }
-            $queue->status = 1;
-            $queue->save();
-            // $this->updateSnapShotJob($queue);
-            $result = $queue->name.' '.'Activated successfully';
-
-            return redirect()->back()->with('success', $result);
-        } catch (Exception $ex) {
-            return redirect()->back()->with('fails', $ex->getMessage());
-        }
-    }
-
-    public function getShortNameById($queueid)
-    {
-        $short = '';
-        $queues = new QueueService();
-        $queue = $queues->find($queueid);
-        if ($queue) {
-            $short = $queue->short_name;
-        }
-
-        return $short;
-    }
-
-    public function getIdByShortName($short)
-    {
-        $id = '';
+public function getIdByShortName($short) {
+        $id = "";
         $queues = new QueueService();
         $queue = $queues->where('short_name', $short)->first();
         if ($queue) {
@@ -162,70 +157,66 @@ class QueueController extends Controller
         return $id;
     }
 
-    public function getFormById($id)
-    {
-        try {
-            $short = $this->getShortNameById($id);
-            $form = '';
-            switch ($short) {
-        case 'beanstalkd':
+public function getFormById($id) {
+    try {
+    $short = $this->getShortNameById($id);
+    $form = "";
+    switch ($short) {
+        case "beanstalkd":
             $form .= "<div class='row'>";
-            $form .= $this->form($short, 'Driver', 'driver', 'col-md-6 form-group', 'beanstalkd');
-            $form .= $this->form($short, 'Host', 'host', 'col-md-6 form-group', 'localhost');
+            $form .= $this->form($short, 'Driver', 'driver', 'col-md-6 form-group','beanstalkd');
+            $form .= $this->form($short, 'Host', 'host', 'col-md-6 form-group','localhost');
             $form .= $this->form($short, 'Queue', 'queue', 'col-md-6 form-group', 'default');
-            $form .= '</div>';
-
+            $form .= "</div>";
             return $form;
-        case 'sqs':
+        case "sqs":
             $form .= "<div class='row'>";
-            $form .= $this->form($short, 'Driver', 'driver', 'col-md-6 form-group', 'sqs');
-            $form .= $this->form($short, 'Key', 'key', 'col-md-6 form-group', 'your-public-key');
+            $form .= $this->form($short, 'Driver', 'driver', 'col-md-6 form-group','sqs');
+            $form .= $this->form($short, 'Key', 'key', 'col-md-6 form-group','your-public-key');
             $form .= $this->form($short, 'Secret', 'secret', 'col-md-6 form-group', 'your-queue-url');
             $form .= $this->form($short, 'Region', 'region', 'col-md-6 form-group', 'us-east-1');
-            $form .= '</div>';
-
+            $form .= "</div>";
             return $form;
-        case 'iron':
+        case "iron":
             $form .= "<div class='row'>";
-            $form .= $this->form($short, 'Driver', 'driver', 'col-md-6 form-group', 'iron');
-            $form .= $this->form($short, 'Host', 'host', 'col-md-6 form-group', 'mq-aws-us-east-1.iron.io');
+            $form .= $this->form($short, 'Driver', 'driver', 'col-md-6 form-group','iron');
+            $form .= $this->form($short, 'Host', 'host', 'col-md-6 form-group','mq-aws-us-east-1.iron.io');
             $form .= $this->form($short, 'Token', 'token', 'col-md-6 form-group', 'your-token');
             $form .= $this->form($short, 'Project', 'project', 'col-md-6 form-group', 'your-project-id');
             $form .= $this->form($short, 'Queue', 'queue', 'col-md-6 form-group', 'your-queue-name');
-            $form .= '</div>';
-
+            $form .= "</div>";
             return $form;
-        case 'redis':
-            if (! extension_loaded('redis')) {
-                return errorResponse(\Lang::get('message.extension_required_error', ['extension' => 'redis']), 500);
+        case "redis":
+            if(!extension_loaded('redis')) {
+            	return errorResponse(\Lang::get('message.extension_required_error', ['extension' => 'redis']), 500);
             }
             $form .= "<div class='row'>";
-            $form .= $this->form($short, 'Driver', 'driver', 'col-md-6 form-group', 'redis');
+            $form .= $this->form($short, 'Driver', 'driver', 'col-md-6 form-group','redis');
             $form .= $this->form($short, 'Queue', 'queue', 'col-md-6 form-group', 'default');
-            $form .= '</div>';
-
+            $form .= "</div>";
             return $form;
-        default:
+        default :
             return $form;
     }
-        } catch (Exception $e) {
-            return errorResponse($e->getMessage());
-        }
+    } catch(Exception $e) {
+    	return errorResponse($e->getMessage());
     }
+	}	
 
-    public function form($short, $label, $name, $class, $placeholder = '')
-    {
+
+	public function form($short, $label, $name, $class, $placeholder = '') {
+
         $queueid = $this->getIdByShortName($short);
         $queues = new QueueService();
         $queue = $queues->find($queueid);
         if ($queue) {
-            $form = "<div class='".$class."'>".Form::label($name, $label)."<span class='text-red'> *</span>".
-                    Form::text($name, $queue->getExtraField($name), ['class' => 'form-control', 'placeholder' => $placeholder]).'</div>';
-        } else {
-            $form = "<div class='".$class."'>".Form::label($name, $label)."<span class='text-red'> *</span>".
-                    Form::text($name, null, ['class' => 'form-control', 'placeholder' => $placeholder]).'</div>';
-        }
 
+            $form = "<div class='" . $class . "'>" . Form::label($name, $label) . "<span class='text-red'> *</span>" .
+                    Form::text($name, $queue->getExtraField($name), ['class' => "form-control", 'placeholder' => $placeholder]) . "</div>";
+        } else {
+            $form = "<div class='" . $class . "'>" . Form::label($name, $label) . "<span class='text-red'> *</span>" .
+                    Form::text($name, NULL, ['class' => "form-control", 'placeholder' => $placeholder]) . "</div>";
+        }
         return $form;
     }
 }
--- app/Plugins/Stripe/Controllers/SettingsController.php
+++ app/Plugins/Stripe/Controllers/SettingsController.php
@@ -220,8 +220,11 @@ class SettingsController extends Controller
     {
         $setting = Setting::find(1);
         $paymentFailData = 'Payment for'.' '.'of'.' '.\Auth::user()->currency.' '.$amount.' '.'failed by'.' '.\Auth::user()->first_name.' '.\Auth::user()->last_name.' '.'. User Email:'.' '.\Auth::user()->email.'<br>'.'Reason:'.$exceptionMessage;
-        $mail = new \App\Http\Controllers\Common\PhpMailController();
-        $mail->sendEmail($setting->email, $setting->company_email, $paymentFailData, 'Payment failed ');
+
+            $mail = new \App\Http\Controllers\Common\PhpMailController();
+            $mail->sendEmail($setting->email, $setting->company_email, $paymentFailData, 'Payment failed ');
+        
+
     }
 
     public static function sendPaymentSuccessMailtoAdmin($currency, $total, $user, $productName)
@@ -230,9 +233,9 @@ class SettingsController extends Controller
         $templateController = new \App\Http\Controllers\Common\TemplateController();
         $paymentSuccessdata = 'Payment for'.' '.$productName.' '.'of'.' '.$currency.' '.$total.' '.'successful by'.' '.$user->first_name.' '.$user->last_name.' '.'Email:'.' '.$user->email;
 
-
         $mail = new \App\Http\Controllers\Common\PhpMailController();
-        $mail->sendEmail($setting->email, $setting->company_email, $paymentSuccessdata, 'Payment Successful ');
+            $mail->sendEmail($setting->email, $setting->company_email, $paymentSuccessdata, 'Payment Successful ');
+       
     }
 
 

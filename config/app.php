a412ee9ad36206c06f8dfab9947e602d9ba7764a
Ashutosh pathak <ashutosh.pathak@ladybirdweb.com>
2020-09-03 18:26:46 +0530

    horizon config added

--- config/app.php
+++ config/app.php
@@ -191,6 +191,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\HorizonServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
         Collective\Html\HtmlServiceProvider::class,

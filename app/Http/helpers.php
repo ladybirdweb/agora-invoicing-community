dbbce72a5c624f7484e18a8db31a2ed8504db57a
Ashutosh pathak <ashu555@users.noreply.github.com>
2020-09-03 10:47:30 +0000

    Apply fixes from StyleCI

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -178,7 +178,6 @@ function getStatusLabel($status, $badge = 'badge')
     }
 }
 
-
 function currencyFormat($amount = null, $currency = null, $include_symbol = true)
 {
     if ($currency == 'INR') {
@@ -225,27 +224,25 @@ function getIndianCurrencyFormat($number)
 
 /**
  * sets mail config and reloads the config into the container
- * NOTE: this is getting used outside the class to set service config
+ * NOTE: this is getting used outside the class to set service config.
  * @return void
  */
 function setServiceConfig($emailConfig)
 {
     $sendingProtocol = $emailConfig->driver;
-    if($sendingProtocol && $sendingProtocol != 'smtp' && $sendingProtocol != 'mail'){
-
+    if ($sendingProtocol && $sendingProtocol != 'smtp' && $sendingProtocol != 'mail') {
         $services = \Config::get("services.$sendingProtocol");
         $dynamicServiceConfig = [];
 
         //loop over it and assign according to the keys given by user
-        foreach($services as $key => $value){
+        foreach ($services as $key => $value) {
             $dynamicServiceConfig[$key] = isset($emailConfig[$key]) ? $emailConfig[$key] : $value;
         }
 
         //setting that service configuration
         \Config::set("services.$sendingProtocol", $dynamicServiceConfig);
-    }
-    else {
-        \Config::set('mail.host',$emailConfig['host']);
+    } else {
+        \Config::set('mail.host', $emailConfig['host']);
         \Config::set('mail.port', $emailConfig['port']);
         \Config::set('mail.password', $emailConfig['password']);
         \Config::set('mail.security', $emailConfig['encryption']);
@@ -254,16 +251,16 @@ function setServiceConfig($emailConfig)
     //setting mail driver as $sending protocol
     \Config::set('mail.driver', $sendingProtocol);
     \Config::set('mail.from.address', $emailConfig['email']);
-    \Config::set('mail.from.name',  $emailConfig['company']);
+    \Config::set('mail.from.name', $emailConfig['company']);
     \Config::set('mail.username', $emailConfig['email']);
 
     //setting the config again in the service container
     (new \Illuminate\Mail\MailServiceProvider(app()))->register();
-
 }
 
 function persistentCache($key, Closure $closure, $noOfSeconds = 30, array $variables = [])
 {
     $keySalt = json_encode($variables);
+
     return Cache::remember($key.$keySalt, $noOfSeconds, $closure);
 }

09d9eb4dbdb8f780e3cf4d5d58c5ed1aaee10435
Ashutosh pathak <ashutosh.pathak@ladybirdweb.com>
2020-09-08 17:45:56 +0530

    Fixes #1320

--- app/Http/Controllers/Installer/DatabaseController.php
+++ app/Http/Controllers/Installer/DatabaseController.php
@@ -4,22 +4,16 @@ namespace App\Http\Controllers\Installer;
 
 use RachidLaasri\LaravelInstaller\Controllers\DatabaseController as BaseDatabaseController;
 use RachidLaasri\LaravelInstaller\Helpers\DatabaseManager;
+use Exception;
+use Illuminate\Database\SQLiteConnection;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\Console\Output\BufferedOutput;
 
 class DatabaseController extends BaseDatabaseController
 {
-    /**
-     * @var DatabaseManager
-     */
-    private $databaseManager;
-
-    /**
-     * @param DatabaseManager $databaseManager
-     */
-    public function __construct(DatabaseManager $databaseManager)
-    {
-        $this->databaseManager = $databaseManager;
-    }
-
+    
     /**
      * Migrate and seed the database.
      *
@@ -28,13 +22,95 @@ class DatabaseController extends BaseDatabaseController
     public function database()
     {
         try {
-            $response = $this->databaseManager->migrateAndSeed();
+            $response = $this->migrateAndSeed();
 
             return redirect()->route('AgoraInstaller::final')
                          ->with(['message' => $response]);
         } catch (\Exception $ex) {
             return redirect()->route('AgoraInstaller::environmentWizard')
                          ->with(['fails' =>$ex->getMessage()]);
+        }
+    }
+
+    /**
+     * Migrate and seed the database.
+     *
+     * @return array
+     */
+    public function migrateAndSeed()
+    {
+        $outputLog = new BufferedOutput;
+
+        $this->sqlite($outputLog);
+
+        return $this->migrate($outputLog);
+    }
+
+    /**
+     * Run the migration and call the seeder.
+     *
+     * @param \Symfony\Component\Console\Output\BufferedOutput $outputLog
+     * @return array
+     */
+    private function migrate(BufferedOutput $outputLog)
+    {
+        try {
+            Artisan::call('migrate', ['--force'=> true], $outputLog);
+        } catch (Exception $e) {
+             throw new Exception($e->getMessage());
+        }
+
+        return $this->seed($outputLog);
+    }
+
+    /**
+     * Seed the database.
+     *
+     * @param \Symfony\Component\Console\Output\BufferedOutput $outputLog
+     * @return array
+     */
+    private function seed(BufferedOutput $outputLog)
+    {
+        try {
+            Artisan::call('db:seed', ['--force' => true], $outputLog);
+        } catch (Exception $e) {
+             throw new Exception($e->getMessage());
+        }
+
+        return $this->response(trans('installer_messages.final.finished'), 'success', $outputLog);
+    }
+
+    /**
+     * Return a formatted error messages.
+     *
+     * @param string $message
+     * @param string $status
+     * @param \Symfony\Component\Console\Output\BufferedOutput $outputLog
+     * @return array
+     */
+    private function response($message, $status, BufferedOutput $outputLog)
+    {
+        return [
+            'status' => $status,
+            'message' => $message,
+            'dbOutputLog' => $outputLog->fetch(),
+        ];
+    }
+
+    /**
+     * Check database type. If SQLite, then create the database file.
+     *
+     * @param \Symfony\Component\Console\Output\BufferedOutput $outputLog
+     */
+    private function sqlite(BufferedOutput $outputLog)
+    {
+        if (DB::connection() instanceof SQLiteConnection) {
+            $database = DB::connection()->getDatabaseName();
+            if (! file_exists($database)) {
+                touch($database);
+                DB::reconnect(Config::get('database.default'));
+            }
+            $outputLog->write('Using SqlLite database: '.$database, 1);
         }
     }
 }

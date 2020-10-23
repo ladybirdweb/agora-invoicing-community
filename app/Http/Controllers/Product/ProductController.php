fdf414ec2a1d88da26c3ab89704ffb9ccfccbe31
Ashutosh pathak <ashu555@users.noreply.github.com>
2020-10-23 09:32:42 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -176,7 +176,7 @@ class ProductController extends BaseProductController
                 'producttitle'  => 'required',
                 'version'      => 'required',
                 'filename'      => 'required',
-                'dependencies'  =>'required'
+                'dependencies'  =>'required',
             ],
        ['filename.required' => 'Please Uplaod A file',
        ]

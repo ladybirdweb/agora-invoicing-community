770e076e4c7394d29680a5220733d079b934c43e
Ashutosh pathak <ashu555@users.noreply.github.com>
2020-12-21 04:50:16 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Tenancy/TenantController.php
+++ app/Http/Controllers/Tenancy/TenantController.php
@@ -2,67 +2,63 @@
 
 namespace App\Http\Controllers\Tenancy;
 
-use App\ThirdPartyApp;
-use GuzzleHttp\Client;
-use Carbon\Carbon;
-use App\Model\Order\Order;
 use App\Http\Controllers\Controller;
+use App\Model\Order\Order;
+use App\ThirdPartyApp;
+use Carbon\Carbon;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
 class TenantController extends Controller
 {
-   
-	public function __construct(Client $client)
+    public function __construct(Client $client)
     {
-    	$this->client = $client;
+        $this->client = $client;
         $this->url = 'http://faveo.helpdesk';
     }
 
-
     /**
-     * Complete logic for creating new tenant is handled here
+     * Complete logic for creating new tenant is handled here.
      */
     public function createTenant(Request $request)
     {
         $this->validate($request,
                 [
-                'orderNo' => 'required',
-                'domain'=>  'required',
-            ]);
+                    'orderNo' => 'required',
+                    'domain'=>  'required',
+                ]);
         try {
-            $licCode = Order::where('number',$request->input('orderNo'))->first()->serial_key;
-            $keys = ThirdPartyApp::where('app_name','faveo_app_key')->select('app_key', 'app_secret')->first();
-            if(!$keys->app_key) {//Valdidate if the app key to be sent is valid or not
+            $licCode = Order::where('number', $request->input('orderNo'))->first()->serial_key;
+            $keys = ThirdPartyApp::where('app_name', 'faveo_app_key')->select('app_key', 'app_secret')->first();
+            if (! $keys->app_key) {//Valdidate if the app key to be sent is valid or not
                 throw new Exception('Invalid App key provided. Please contact admin.');
-                
             }
             $token = str_random(32);
             \DB::table('third_party_tokens')->insert(['user_id'=>\Auth::user()->id, 'token'=>$token]);
             $user = \Auth::user()->email;
             $client = new Client([]);
-            $data = ['domain' => $request->input('domain'), 'app_key'=>$keys->app_key, 'token'=>$token, 'lic_code'=>$licCode, 'username'=>$user,'userId'=>\Auth::user()->id,'timestamp'=>Carbon::now()->timestamp];
-           
+            $data = ['domain' => $request->input('domain'), 'app_key'=>$keys->app_key, 'token'=>$token, 'lic_code'=>$licCode, 'username'=>$user, 'userId'=>\Auth::user()->id, 'timestamp'=>Carbon::now()->timestamp];
+
             $encodedData = http_build_query($data);
             $hashedSignature = hash_hmac('sha256', $encodedData, $keys->app_secret);
 
             $response = $client->request(
                     'POST',
-                    'http://faveo.helpdesk/tenants',['form_params'=>$data, 'headers'=>['signature'=>$hashedSignature]]
+                    'http://faveo.helpdesk/tenants', ['form_params'=>$data, 'headers'=>['signature'=>$hashedSignature]]
                 );
-             var_dump($response);
-             die();
+            var_dump($response);
+            exit();
             // dd(json_decode($response->getBody()->getContents()));
             // dd($response);
             // json_decode($response->getBody()->getContents()
             return successResponse('Created successfully');
-          
         } catch (Exception $e) {
             // var_dump($e);
             // die();
             dd($e);
+
             return errorResponse($e->getMessage());
         }
-            
     }
 
     public function verifyThirdPartyToken(Request $request)
@@ -70,20 +66,16 @@ class TenantController extends Controller
         try {
             $token = $request->input('token');
             $userId = $request->input('userId');
-            $faveoToken = \DB::table('third_party_tokens')->where('user_id',$userId)->value('token');
-            if($faveoToken && $token == $faveoToken)
-            {
-                \DB::table('third_party_tokens')->where('user_id',$userId)->delete();
+            $faveoToken = \DB::table('third_party_tokens')->where('user_id', $userId)->value('token');
+            if ($faveoToken && $token == $faveoToken) {
+                \DB::table('third_party_tokens')->where('user_id', $userId)->delete();
                 //delete third party token here
                 return successResponse('Valid token');
             } else {
                 return errorResponse('Invalid Token');
             }
-            
         } catch (Exception $e) {
             throw new \Exception($e->getMessage());
-            
         }
-        
     }
 }

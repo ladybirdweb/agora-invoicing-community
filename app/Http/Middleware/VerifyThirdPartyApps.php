d20a61407753ea4a0b0bc5e6e90e8337a7e0e88b
Ashutosh pathak <ashu555@users.noreply.github.com>
2021-01-08 13:36:47 +0000

    Apply fixes from StyleCI

--- app/Http/Middleware/VerifyThirdPartyApps.php
+++ app/Http/Middleware/VerifyThirdPartyApps.php
@@ -18,17 +18,17 @@ class VerifyThirdPartyApps
     {
         $currentTimestamp = time();
         $timestampDifference = $currentTimestamp - $request->input('timestamp');
-        if(!is_numeric($timestampDifference) ||  $timestampDifference > 900) //900 sec is 15 mins
-        {
-              $result = ['status' => 'fails', 'message' => 'Suspicious activity detected'];
-              return response()->json(compact('result'));
+        if (! is_numeric($timestampDifference) || $timestampDifference > 900) { //900 sec is 15 mins
+            $result = ['status' => 'fails', 'message' => 'Suspicious activity detected'];
+
+            return response()->json(compact('result'));
         }
         $appKey = $request->header('app_key');
         $requestParameters = file_get_contents('php://input'); //get all the form parameters in the request
         $requestHeader = $request->header('signature'); //get signature sent in the request
         $app_secret = $keys = ThirdPartyApp::where('app_name', 'faveo_app_key')->where('app_key', $appKey)->value('app_secret');
         $signature = hash_hmac('sha256', $requestParameters, $app_secret); //hash the request parameter with the app secret
-         if ($requestHeader && hash_equals($signature, $requestHeader)) {
+        if ($requestHeader && hash_equals($signature, $requestHeader)) {
             return $next($request);
         } else {
             $result = ['status' => 'fails', 'message' => 'Invalid signature'];

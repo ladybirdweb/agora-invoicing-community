eef2e22ac7f9f0510f99d7d3b8a17276ef53c519
Ashutosh pathak <ashutosh.pathak@ladybirdweb.com>
2021-02-01 14:53:25 +0530

    updated version

--- app/Http/Controllers/BaseHomeController.php
+++ app/Http/Controllers/BaseHomeController.php
@@ -194,11 +194,11 @@ class BaseHomeController extends Controller
                 $installationDetails = $cont->searchInstallationPath($orderForLicense->first()->serial_key, $orderForLicense->first()->product);
                 foreach ($installationDetails['installed_path'] as $path) {
                     $ipAndDomain = explode(',', $path);
-                    InstallationDetail::updateOrCreate(['installation_path'=>$ipAndDomain[0], 'installation_ip'=>$ipAndDomain[1]], ['installation_path'=>$ipAndDomain[0], 'installation_ip'=>$ipAndDomain[1], 'version'=>$request->input('version'), 'order_id'=>$orderForLicense->first()->id]);
+                    InstallationDetail::updateOrCreate(['installation_path'=>$ipAndDomain[0], 'installation_ip'=>$ipAndDomain[1]], ['last_active'=> (string) \Carbon\Carbon::now(),'installation_path'=>$ipAndDomain[0], 'installation_ip'=>$ipAndDomain[1], 'version'=>$request->input('version'), 'order_id'=>$orderForLicense->first()->id]);
                 }
                 $existingVersion = Subscription::where('order_id', $orderForLicense->first()->id)->value('version');
-                if ($existingVersion && $request->input('version') > $existingVersion) {
-                    Subscription::where('order_id', $orderForLicense->first()->id)->update(['version'=>$request->input('version')]);
+                if ($existingVersion) {
+                    Subscription::where('order_id', $orderForLicense->first()->id)->update(['version'=>$request->input('version'), 'version_updated_at'=> (string) \Carbon\Carbon::now()]);
                 }
 
                 return ['status' => 'success', 'message' => 'version-updated-successfully'];

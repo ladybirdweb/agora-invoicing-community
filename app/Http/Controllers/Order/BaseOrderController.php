1d1110663aa1ec42604588b443509064101b4c2a
Sandesh Menath <79452740+sandesh556@users.noreply.github.com>
2021-07-26 14:38:57 +0530

    license manager integration completed

--- app/Http/Controllers/Order/BaseOrderController.php
+++ app/Http/Controllers/Order/BaseOrderController.php
@@ -155,8 +155,8 @@ class BaseOrderController extends ExtendedOrderController
             $updatesExpiry = $this->getUpdatesExpiryDate($permissions['generateUpdatesxpiryDate'], $days);
             $supportExpiry = $this->getSupportExpiryDate($permissions['generateSupportExpiryDate'], $days);
             $user_id = $this->order->find($orderid)->client;
-            $this->subscription->create(['user_id'     => $user_id,
-                'plan_id' => $planid, 'order_id' => $orderid, 'update_ends_at' =>$updatesExpiry, 'ends_at' => $licenseExpiry, 'support_ends_at'=>$supportExpiry, 'version'=> $version, 'product_id' =>$product, ]);
+            $this->subscription->create(['user_id' => $user_id,
+                'plan_id' => $planid, 'order_id' => $orderid, 'update_ends_at' =>$updatesExpiry, 'ends_at' => $licenseExpiry, 'support_ends_at'=>$supportExpiry, 'version'=> $version, 'product_id'=>$product, ]);
 
             $licenseStatus = StatusSetting::pluck('license_status')->first();
             if ($licenseStatus == 1) {

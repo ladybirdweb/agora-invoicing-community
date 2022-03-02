9c9e12a2e94cc57256a3ed56c0ff24d76e6d0191
sowmiyasubramaniyan <sowmiya.subramaniyan@ladybirdweb.com>
2022-03-02 09:53:14 +0530

    free trail added

--- resources/views/themes/default1/layouts/front/master.blade.php
+++ resources/views/themes/default1/layouts/front/master.blade.php
@@ -447,7 +447,12 @@ if($script->on_every_page == 1) {
 
  @auth
 
+<?php
+    $userId = \Auth::user()->id;
+    $orderNo = DB::table('orders')->where('client',$userId)->value('number');
 
+    
+?>
 <div class="modal fade" id="tenant" data-backdrop="static" data-keyboard="false">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -465,11 +470,12 @@ if($script->on_every_page == 1) {
 
             <div class="container">
                 <form action="" method="post" style="width:500px; margin: auto auto;" class="card card-body">
+                    <input type="hidden" id="orderNo" name="order" value={{$orderNo}}>
                     <div class="form-group">
                         <label>Domain</label>
                         <div class="row" style="margin-left: 2px; margin-right: 2px;">
-                            
-                            <input  type="text"   name="domain" autocomplete="off" id= "userdomain"  class="form-control col col-4" placeholder="Domain" required>
+                             <input type="hidden"  name="order" id="orderId" value="{{$orderNo}}"/>
+                            <input  type="text" name="domain" autocomplete="off" id= "userdomain"  class="form-control col col-4" placeholder="Domain" required>
                             <input type="text" class="form-control col col-8" value=".faveocloud.com" disabled="true">
                         </div>
                     </div>
@@ -481,7 +487,7 @@ if($script->on_every_page == 1) {
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default pull-left closebutton" id="closebutton" data-dismiss="modal"><i class="fa fa-times">&nbsp;&nbsp;</i>Close</button>
-                 <button type="submit" data-id=""  class="btn btn-primary createTenant" id="createTenant" onclick="firstlogin({{Auth::user()->id}})"><i class="fa fa-check">&nbsp;&nbsp;</i>Submit</button>
+                 <button type="submit" data-id="{{$orderNo}}"  class="btn btn-primary createTenant" id="createTenant" onclick="firstlogin({{Auth::user()->id}})"><i class="fa fa-check">&nbsp;&nbsp;</i>Submit</button>
                 {!! Form::close()  !!}
             </div>
             <!-- /Form -->
@@ -824,6 +830,89 @@ if($script->on_every_page == 1) {
                         }
                     });
                   }
+
+                  
+
+    function firstlogin(id) {
+             $('#createTenant').attr('disabled',true)
+                $("#createTenant").html("<i class='fas fa-circle-notch fa-spin'></i>Please Wait...");
+                var domain = $('#userdomain').val();
+                var password = $('#password').val();
+                var order = $('#orderId').val();
+               $.ajax({
+                    type: 'POST',
+                    data: {'id':id},
+                    url: "{{url('firstlogin')}}",
+                    success:function()
+                    {
+                        //location.reload();
+                    }
+                });
+           $.ajax({
+             url: "{{url('create/tenant')}}",
+            type: "POST",
+            data: {'domain': domain, 'password': password, 'orderNo': order},
+            success: function (data) {
+                $('#createTenant').attr('disabled',false)
+                $("#createTenant").html("<i class='fa fa-check'>&nbsp;&nbsp;</i>Submit");
+                if(data.status == 'validationFailure') {
+                    var html = '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>Whoops! </strong>Something went wrong<ul>';
+                    for (var key in data.message)
+                    {
+                        html += '<li>' + data.message[key][0] + '</li>'
+                    }
+                     html += '</ul></div>';
+                 $('#error').show();
+                  $('#success').hide();
+                  document.getElementById('error').innerHTML = html;
+                } else if(data.status == 'false') {
+                    $('#error').show();
+                    $('#success').hide();
+                    var result =  '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Whoops! </strong>Something went wrong!!<br><ul><li>'+data.message+'</li></ul></div>';
+                        $('#error').html(result);
+                    } else if(data.status == 'success_with_warning') {
+                        console.log('here');
+                    $('#error').show();
+                    $('#success').hide();
+                    var result =  '<div class="alert alert-warning alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Whoops! </strong><br><ul><li>'+data.message+'</li></ul></div>';
+                        $('#error').html(result);
+                    } else {
+                        $('#error').hide();
+                        $('#success').show();
+                       var result =  '<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong><i class="fa fa-check"></i>Success! </strong>'+data.message+'!</div>';
+                    $('#success').html(result);
+                    }
+            },error: function (response) {
+                $('#createTenant').attr('disabled',false)
+                $("#createTenant").html("<i class='fa fa-check'>&nbsp;&nbsp;</i>Submit");
+                 $("#generate").html("<i class='fa fa-check'>&nbsp;&nbsp;</i>Submit");
+                if(response.status == 422) {
+                     var html = '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>Whoops! </strong>Something went wrong<ul>';
+                    for (var key in response.responseJSON.errors)
+                    {
+                        html += '<li>' + response.responseJSON.errors[key][0] + '</li>'
+                    }
+                } else {
+                    var html = '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>Whoops! </strong>Something went wrong<ul>';
+                    html += '<li>' + response.responseJSON.message + '</li>'
+                }
+                
+                 html += '</ul></div>';
+                 $('#error').show();
+                 $('#success').hide();
+                 document.getElementById('error').innerHTML = html;
+                
+            }
+        });
+            }
+        $(document).on("click", ".open-createTenantDialog", function () {
+    
+     $('#tenant').modal('show');
+});
+    $('.closebutton').on('click',function(){
+        location.reload();
+    });
+
 
 
             $(document).ready(function(){

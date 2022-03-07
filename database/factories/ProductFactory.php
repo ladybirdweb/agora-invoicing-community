41798f5f4e7a29566a020e330b65150c1083df02
sowmiyasubramaniyan <sowmiya.subramaniyan@ladybirdweb.com>
2022-03-07 15:28:34 +0530

    Testcase for Freetrial

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -6,16 +6,14 @@ use Faker\Generator as Faker;
 $factory->define(Product::class, function (Faker $faker) {
     return [
 
-        'name'                => 'Helpdesk Advance',
-        'description'         => 'Few Lines Of Description',
+        'name'                => $faker->name,
+        'description'         => $faker->sentence,
         // 'type'                => 1,
-        'category'            => 'helpdesk',
         'group'               => 1,
         'can_modify_agent'    => 0,
         'can_modify_quantity' => 0,
         'require_domain'      => 1,
         'show_agent'          => 1,
-        'github_owner'        => 'ladybirdweb',
-        'github_repository'   => 'faveo-helpdesk-advance',
+        'product_sku' => 'FAVEOCLOUDBETA',
     ];
 });

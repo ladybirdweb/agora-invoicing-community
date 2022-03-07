80f1c33059e3f9289ab1f42c17d53501afb6872a
sowmiyasubramaniyan <sowmiya.subramaniyan@ladybirdweb.com>
2022-03-07 15:28:34 +0530

    Testcase for Freetrial

--- tests/Unit/FreeTrialControllerTest.php
+++ tests/Unit/FreeTrialControllerTest.php
@@ -3,24 +3,37 @@
 namespace Tests\Unit;
 
 use App\Http\Controllers\FreeTrailController;
-use App\Model\Product\Product;
 use App\User;
+use App\Model\Product\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Mockery;
 use Tests\DBTestCase;
+
+
+
 
 class FreeTrailControllerTest extends DBTestCase
 {
-    public function test_firstLoginAttempt_return_exception_when_not_first_time_register_users()
+
+    public function test_firstloginatem_generateinvoiceorder_returnstatus200()
     {
-        $this->expectException(\Exception::class);
-        $user = User::factory()->create(['role' => 'user', 'country' => 'IN']);
-        Product::factory()->create();
-        $auth = Auth::loginUsingId($user->id);
-        $this->actingAs($user);
-        $response = (new FreeTrailController())->firstLoginAttempt(new Request(['id' => $user->id, 'first_time_login' => 1]));
-        $this->expectExceptionMessage('Can not Generate Freetrial Cloud instance');
-        $response = $response->getOriginalContent();
         $this->assertFalse(auth()->check());
+        $user = factory(User::class)->create(['role' => 'user', 'country' => 'IN']);
+        $product = factory(Product::class)->create();
+        $auth = Auth::loginUsingId($user->id);
+        $this->assertEquals($user->id, $auth->id);
+        $this->assertEquals(0,$auth->first_time_login);
+        $this->actingAs($user);
+        $response = (new FreeTrailController())->firstloginatem(new Request(['id' => $user->id]));
+        $response=$response->getOriginalContent();
+        $this->assertEquals('Invoice and Order has been generated successfully',$response['message']);
+        $this->assertEquals(true,$response['success']);
+
+
+
     }
+
+
+
 }

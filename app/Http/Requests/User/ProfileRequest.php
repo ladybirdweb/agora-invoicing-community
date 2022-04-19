63fab3ea3e140aa23abc6f7cae8b88126a2e2109
sowmiyasubramaniyan <sowmiya.subramaniyan@ladybirdweb.com>
2022-04-19 16:26:07 +0530

    create login pop

--- app/Http/Requests/User/ProfileRequest.php
+++ app/Http/Requests/User/ProfileRequest.php
@@ -23,18 +23,19 @@ class ProfileRequest extends Request
      */
     public function rules()
     {
+
+        $value = $this->input('value');
+        
+
         if ($this->segment(1) == 'profile') {
             $userid = \Auth::user()->id;
 
             return [
                 'first_name'             => 'required',
                 'last_name'              => 'required',
-                'company'                => 'required|max:50',
                 'email'                  => 'required',
                 'mobile'                 => 'required',
-                'address'                => 'required',
                 'user_name'              => 'unique:users,user_name,'.$userid,
-                'state'                  => 'required_if:country,IN',
                 'timezone_id'            => 'required',
                 'profile_pic'            => 'sometimes|mimes:jpeg,jpg,png,gif|max:100000',
 
@@ -49,11 +50,8 @@ class ProfileRequest extends Request
                 'last_name'              => 'required|max:30',
                 'mobile'                => 'required|regex:/[0-9]/|min:5|max:20',
                 'email'                 => 'required',
-                'company'               => 'required|max:50',
-                'address'                => 'required',
                 'mobile'                 => 'required',
                 'country'                => 'required|exists:countries,country_code_char2',
-                'state'                  => 'required_if:country,IN',
                 'profile_pic'            => 'sometimes|mimes:jpeg,jpg,png,gif|max:100000',
 
             ];
@@ -66,27 +64,36 @@ class ProfileRequest extends Request
             ];
         }
 
-        if ($this->segment(1) == 'auth') {
+        if ($this->segment(1) == 'auth' && $value == '0') {
             return [
                 'first_name'            => 'required|min:2|max:30',
                 'last_name'             => 'required|max:30',
-                'email'                 => 'required|email|unique:users,email|unique:settings,company_email|unique:settings,email',
-                'company'               => 'required|max:50',
+
+                'email'                 => 'required|email|unique:users',
+
                 'mobile'                => 'required',
-                'address'                => 'required',
                 'terms'                 => 'accepted',
-                'password'              => 'required|min:6',
-                'password_confirmation' => 'required|same:password',
                 'country'               => 'required|exists:countries,country_code_char2',
             ];
+           
+        }
+         if ($this->segment(1) == 'auth' && $value == '1') {
+            return [
+                'first_name'            => 'required|min:2|max:30',
+                'last_name'             => 'required|max:30',
+                'email'                 => 'required|email|unique:users',
+                'mobile'                => 'required',
+                'logterms'                 => 'accepted',
+                'country'               => 'required|exists:countries,country_code_char2',
+            ];
+           
         }
     }
 
     public function messages()
     {
         return[
-            'mobile_code.required'           => 'Enter Country code (mobile)',
-            'state.required_if'           => 'The state field is required when country is India.',
+            // 'mobile_code.required'           => 'Enter Country code (mobile)',
         ];
     }
 }

81dbe97ee5e1228c1101474830477dcf654a0aaf
sowmiyasubramaniyan <sowmiya.subramaniyan@ladybirdweb.com>
2022-05-06 17:02:43 +0530

    Done code correction

--- tests/Unit/FreeTrialControllerTest.php
+++ tests/Unit/FreeTrialControllerTest.php
@@ -18,15 +18,15 @@ class FreeTrailControllerTest extends DBTestCase
 
     public function test_firstLoginAtem_generateinvoiceorder_returnstatus200()
     {
-        $this->assertFalse(auth()->check());
         $user = factory(User::class)->create(['role' => 'user', 'country' => 'IN']);
         factory(Product::class)->create();
         $auth = Auth::loginUsingId($user->id);
-        $this->assertEquals($user->id, $auth->id);
-        $this->assertEquals(0,$auth->first_time_login);
         $this->actingAs($user);
         $response = (new FreeTrailController())->firstLoginAtem(new Request(['id' => $user->id]));
         $response=$response->getOriginalContent();
+        $this->assertFalse(auth()->check());
+        $this->assertEquals($user->id, $auth->id);
+        $this->assertEquals(0,$auth->first_time_login);
         $this->assertEquals('Invoice and Order has been generated successfully',$response['message']);
         $this->assertEquals(true,$response['success']);
 

3edc9a741b9d4b759a3506ba5d148b93f903f4df
Sandesh Menath <79452740+sandesh556@users.noreply.github.com>
2022-06-08 12:40:12 +0530

    few changes for live cloud

--- resources/views/themes/default1/front/clients/deploy-popup.blade.php
+++ resources/views/themes/default1/front/clients/deploy-popup.blade.php
@@ -1,68 +1,75 @@
-<a data-id="{{$orderNumber}}" href="#tenant" class="btn  btn-primary btn-xs open-createTenantDialog" data-toggle="modal"><i class="fa fa-refresh"></i>&nbsp;Deploy</a>
+<a data-id="{{$orderNumber}}" href="#tenant" class="btn  btn-primary btn-xs open-createTenantDialog" data-toggle="modal"><i class="fa fa-edit"></i>&nbsp;Change Cloud Domain</a>
 <div class="modal fade" id="tenant" data-backdrop="static" data-keyboard="false">
     <div class="modal-dialog">
         <div class="modal-content">
             {!! Form::open() !!}
             <div class="modal-header">
-                 <h4 class="modal-title">Create an instance</h4>
+                <h4 class="modal-title">Do you want to change your existing faveo cloud domain?</h4>
             </div>
-             
+
             <div class="modal-body">
                 <div id="success">
-                 </div>
-                  <div id="error">
-                 </div>
+                </div>
+                <div id="error">
+                </div>
                 <!-- Form  -->
-                
-            <div class="container">
-                <form action="" method="post" style="width:500px; margin: auto auto;" class="card card-body">
-                    <input type="hidden" id="orderNo" name="order" value={{$orderNumber}}>
-                    <div class="form-group">
-                        <label>Domain</label>
-                        <div class="row" style="margin-left: 2px; margin-right: 2px;">
-                             <input type="hidden"  name="order" id="orderId" value=""/>
-                            <input  type="text" name="domain" autocomplete="off" id= "userdomain"  class="form-control col col-4" placeholder="Domain">
-                            <input type="text" class="form-control col col-8" value=".faveocloud.com" disabled="true">
+
+                <div class="container">
+                    <form action="" method="post" style="width:500px; margin: auto auto;" class="card card-body">
+                        <input type="hidden" id="orderNo" name="order" value={{$orderNumber}}>
+                        <div class="form-group">
+                            <label>Enter current domain</label>
+                            <div class="row" style="margin-left: 2px; margin-right: 2px;">
+                                <input type="hidden"  name="order" id="orderId" value=""/>
+                                <input  type="text" name="userdomain" autocomplete="off" id= "userdomain"  class="form-control col col-12" placeholder="billing.faveocloud.com">
+                                <!-- <input type="text" class="form-control col col-8" value=".faveocloud.com" disabled="true">-->
+                            </div>
+                            <br>
+                            <label>Enter new domain</label>
+                            <div class="row" style="margin-left: 2px; margin-right: 2px;">
+                                <input type="hidden"  name="order" id="orderId" value=""/>
+                                <input  type="text" name="usernewdomain" autocomplete="off" id= "usernewdomain"  class="form-control col col-12" placeholder="billing.faveocloud.com">
+                                <!-- <input type="text" class="form-control col col-8" value=".faveocloud.com" disabled="true">-->
+                            </div>
                         </div>
-                    </div>
-                </form>
-            </div>
+                    </form>
+                </div>
 
 
 
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default pull-left closebutton" id="closebutton" data-dismiss="modal"><i class="fa fa-times">&nbsp;&nbsp;</i>Close</button>
-                 <button type="submit"  class="btn btn-primary createTenant" id="createTenant" onclick="createTenant()"><i class="fa fa-check">&nbsp;&nbsp;</i>Submit</button>
+                <button type="submit"  class="btn btn-primary createTenant" id="createTenant" onclick="changeTenantDomain()"><i class="fa fa-check">&nbsp;&nbsp;</i>Submit</button>
                 {!! Form::close()  !!}
             </div>
             <!-- /Form -->
         </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->  
-  
+</div><!-- /.modal -->
+
 <script>
     $(document).on("click", ".open-createTenantDialog", function () {
-     var orderId = $(this).data('id');
-     $(".modal-body #orderId").val( orderId );
-     $('#tenant').modal('show');
-});
+        var orderId = $(this).data('id');
+        $(".modal-body #orderId").val( orderId );
+        $('#tenant').modal('show');
+    });
 
 
     $('.closebutton').on('click',function(){
         location.reload();
     });
 
-    function createTenant(){
+    function changeTenantDomain(){
         $('#createTenant').attr('disabled',true)
         $("#createTenant").html("<i class='fas fa-circle-notch fa-spin'></i>Please Wait...");
         var domain = $('#userdomain').val();
-        var password = $('#password').val();
+        var domainNew = $('#usernewdomain').val();
         var order = $('#orderId').val();
         $.ajax({
-            url: "{{url('create/tenant')}}",
+            url: "{{url('change/domain')}}",
             type: "POST",
-            data: {'domain': domain, 'password': password, 'orderNo': order},
+            data: {'currentDomain': domain, 'newDomain': domainNew, 'orderNo': order},
             success: function (data) {
                 $('#createTenant').attr('disabled',false)
                 $("#createTenant").html("<i class='fa fa-check'>&nbsp;&nbsp;</i>Submit");
@@ -73,34 +80,34 @@
                     {
                         html += '<li>' + data.message[key][0] + '</li>'
                     }
-                     html += '</ul></div>';
-                 $('#error').show();
-                  $('#success').hide();
-                  document.getElementById('error').innerHTML = html;
+                    html += '</ul></div>';
+                    $('#error').show();
+                    $('#success').hide();
+                    document.getElementById('error').innerHTML = html;
                 } else if(data.status == 'false') {
                     $('#error').show();
                     $('#success').hide();
                     var result =  '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Whoops! </strong>Something went wrong!!<br><ul><li>'+data.message+'</li></ul></div>';
-                        $('#error').html(result);
-                    } else if(data.status == 'success_with_warning') {
-                        console.log('here');
+                    $('#error').html(result);
+                } else if(data.status == 'success_with_warning') {
+                    console.log('here');
                     $('#error').show();
                     $('#success').hide();
                     var result =  '<div class="alert alert-warning alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Whoops! </strong><br><ul><li>'+data.message+'</li></ul></div>';
-                        $('#error').html(result);
-                    } else {
-                        $('#error').hide();
-                        $('#success').show();
-                       var result =  '<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong><i class="fa fa-check"></i>Success! </strong>'+data.message+'!</div>';
+                    $('#error').html(result);
+                } else {
+                    $('#error').hide();
+                    $('#success').show();
+                    var result =  '<div class="alert alert-success alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong><i class="fa fa-check"></i>Success! </strong>'+data.message+'!</div>';
                     $('#success').html(result);
-                    }
+                }
             },error: function (response) {
                 $('#createTenant').attr('disabled',false)
                 $("#createTenant").html("<i class='fa fa-check'>&nbsp;&nbsp;</i>Submit");
-                 $("#generate").html("<i class='fa fa-check'>&nbsp;&nbsp;</i>Submit");
+                $("#generate").html("<i class='fa fa-check'>&nbsp;&nbsp;</i>Submit");
                 if(response.status == 422) {
 
-                     var html = '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>Whoops! </strong>Something went wrong<ul>';
+                    var html = '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>Whoops! </strong>Something went wrong<ul>';
                     for (var key in response.responseJSON.errors)
                     {
                         html += '<li>' + response.responseJSON.errors[key][0] + '</li>'
@@ -110,14 +117,14 @@
                     var html = '<div class="alert alert-danger alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><strong>Whoops! </strong>Something went wrong<ul>';
                     html += '<li>' + response.responseJSON.message + '</li>'
                 }
-                
-                 html += '</ul></div>';
-                 $('#error').show();
-                  $('#success').hide();
-                  document.getElementById('error').innerHTML = html;
-                
+
+                html += '</ul></div>';
+                $('#error').show();
+                $('#success').hide();
+                document.getElementById('error').innerHTML = html;
+
             }
 
         })
-}    
+    }
 </script>

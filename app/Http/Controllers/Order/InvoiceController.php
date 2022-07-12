1ff1c0a93627748e0c7e87b8e0f09618b43433ae
sowmiyasubramaniyan <sowmiya.subramaniyan@ladybirdweb.com>
2022-07-12 10:07:47 +0530

    added request file for validation

--- app/Http/Controllers/Order/InvoiceController.php
+++ app/Http/Controllers/Order/InvoiceController.php
@@ -23,6 +23,8 @@ use App\Traits\PaymentsAndInvoices;
 use App\Traits\TaxCalculation;
 use App\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\InvoiceRequest;
+
 
 class InvoiceController extends TaxRatesAndCodeExpiryController
 {
@@ -356,17 +358,7 @@ class InvoiceController extends TaxRatesAndCodeExpiryController
      */
     public function invoiceGenerateByForm(InvoiceRequest $request, $user_id = '')
     {
-        $this->validate($request, [
-            'user'      => 'required',
-            'date'      => 'required|date',
-            'domain'    => 'sometimes|nullable|regex:/^(?!:\/\/)(?=.{1,255}$)((.{1,63}\.){1,127}(?![0-9]*$)[a-z0-9-]+\.?)$/i',
-            'plan'      => 'required_if:subscription,true',
-            'price'     => 'required',
-            'product'   => 'required',
-        ], [
-            'plan.required_if' => 'The subscription field is required',
-            'user.required' => 'The clients field is required',
-        ]);
+       
 
         try {
             $agents = $request->input('agents');
--- app/Http/Requests/InvoiceRequest.php
+++ app/Http/Requests/InvoiceRequest.php
@@ -24,15 +24,14 @@ class InvoiceRequest extends FormRequest
     public function rules()
     {
         return [
-            'user' => 'required',
-            'date' => 'required|date',
-            'domain' => 'sometimes|nullable|regex:/^(?!:\/\/)(?=.{1,255}$)((.{1,63}\.){1,127}(?![0-9]*$)[a-z0-9-]+\.?)$/i',
-            'plan' => 'required_if:subscription,true',
-            'price' => 'required',
-            'product' => 'required',
+            'user'      => 'required',
+            'date'      => 'required|date',
+            'domain'    => 'sometimes|nullable|regex:/^(?!:\/\/)(?=.{1,255}$)((.{1,63}\.){1,127}(?![0-9]*$)[a-z0-9-]+\.?)$/i',
+            'plan'      => 'required_if:subscription,true',
+            'price'     => 'required',
+            'product'   => 'required',
         ];
     }
-
     public function messages()
     {
         return[

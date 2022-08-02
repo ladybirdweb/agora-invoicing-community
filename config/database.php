970397bb9c3ee3664af130a433d7b42769ef3cda
Shift <shift@laravelshift.com>
2022-08-02 12:55:54 +0000

    Shift config files

--- config/database.php
+++ config/database.php
@@ -77,7 +77,7 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
-            'schema' => 'public',
+            'search_path' => 'public',
             'sslmode' => 'prefer',
         ],
 

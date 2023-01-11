20b92dd9ce8e103a27e8736355927c8357d732cd
sowmiyasubramaniyan <sowmiya.subramaniyan@ladybirdweb.com>
2023-01-11 17:23:24 +0530

    Double file issue fix

--- resources/views/themes/default1/product/plan/index.blade.php
+++ resources/views/themes/default1/product/plan/index.blade.php
@@ -63,6 +63,8 @@ Plans
 <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
 <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
 <script type="text/javascript">
+
+
         $('#plan-table').DataTable({
             processing: true,
             serverSide: true,

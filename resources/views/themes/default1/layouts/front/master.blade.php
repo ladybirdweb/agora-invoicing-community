a5760e04a8124117b5b242034b43f7c536466583
sowmiyasubramaniyan <sowmiya.subramaniyan@ladybirdweb.com>
2023-01-20 15:59:55 +0530

    Issue fix in welcome email link and reset password link

--- resources/views/themes/default1/layouts/front/master.blade.php
+++ resources/views/themes/default1/layouts/front/master.blade.php
@@ -418,7 +418,6 @@ if($script->on_every_page == 1) {
                 
                  <div class="alert alert-danger alert-dismissable" role="alert">
                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    <strong><i class="fas fa-exclamation-triangle"></i>Oh snap!</strong>
                     {{Session::get('fails')}}
                 </div>
             

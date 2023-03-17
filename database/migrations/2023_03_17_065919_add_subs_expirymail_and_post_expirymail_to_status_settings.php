707701fa67197bfa05eb669113d566e6c312ac77
sowmiyasubramaniyan <sowmiya.subramaniyan@ladybirdweb.com>
2023-03-17 12:52:34 +0530

    Column added

---
+++ database/migrations/2023_03_17_065919_add_subs_expirymail_and_post_expirymail_to_status_settings.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('status_settings', function (Blueprint $table) {
+            $table->boolean('subs_expirymail')->default(0);
+            $table->boolean('post_expirymail')->default(0);
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('status_settings', function (Blueprint $table) {
+            $table->dropColumn(['subs_expirymail', 'post_expirymail']);
+        });
+    }
+};

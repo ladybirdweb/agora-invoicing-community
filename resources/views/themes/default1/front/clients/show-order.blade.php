d82b2ede5d9f677aefd107eb87ad73a36a889af9
Sowmiya Subramanian <96425427+sowmiyasubramaniyan@users.noreply.github.com>
2023-04-04 16:10:01 +0530

    added apilayer key

--- resources/views/themes/default1/front/clients/show-order.blade.php
+++ resources/views/themes/default1/front/clients/show-order.blade.php
@@ -104,7 +104,8 @@ return $randomString;
 }
 $rzp_key = app\ApiKey::where('id', 1)->value('rzp_key');
 $rzp_secret = app\ApiKey::where('id', 1)->value('rzp_secret');
- $api = new Api($rzp_key, $rzp_secret);
+$apilayer_key = app\ApiKey::where('id', 1)->value('apilayer_key');
+$api = new Api($rzp_key, $rzp_secret);
 $displayCurrency = \Auth::user()->currency;
 $symbol = \Auth::user()->currency;
 if ($symbol == 'INR'){

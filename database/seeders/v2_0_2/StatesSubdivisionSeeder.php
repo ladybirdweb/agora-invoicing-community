fbdc6ae49e6e575cea419526131dab25ce62c164
Sowmiya Subramanian <96425427+sowmiyasubramaniyan@users.noreply.github.com>
2023-04-20 11:34:52 +0530

    Revert "Minor changes"
    This reverts commit e92ebf5be1077a9ed672b995f0d61e6d54117699.

--- database/seeders/v2_0_2/StatesSubdivisionSeeder.php
+++ database/seeders/v2_0_2/StatesSubdivisionSeeder.php
@@ -16,7 +16,6 @@ class StatesSubdivisionSeeder extends Seeder
     public function run()
     {
         \DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        \DB::table('states_subdivisions')->truncate();
         \DB::statement('SET FOREIGN_KEY_CHECKS=1;');  
 
         State::create([

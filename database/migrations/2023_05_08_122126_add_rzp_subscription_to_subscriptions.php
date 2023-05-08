2f537ade173e24539eda623f032baf6b476eab78
sowmiyasubramaniyan <sowmiya.subramaniyan@ladybirdweb.com>
2023-05-08 17:59:16 +0530

    razorpay subscription changes

---
+++ database/migrations/2023_05_08_122126_add_rzp_subscription_to_subscriptions.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (! Schema::hasColumn('subscriptions', 'rzp_subscription')) {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->string('rzp_subscription')->default(0);
+        });
+    }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+          $table->dropColumn('rzp_subscription');
+
+        });
+    }
+};

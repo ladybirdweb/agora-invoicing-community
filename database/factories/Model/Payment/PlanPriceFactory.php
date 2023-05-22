387a0179b88a01ec1fc644cf0992fe05565b1972
sowmiyasubramaniyan <sowmiya.subramaniyan@ladybirdweb.com>
2023-05-22 15:55:50 +0530

    product renewal url

---
+++ database/factories/Model/Payment/PlanPriceFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories\Model\Payment;
+use App\Model\Payment\PlanPrice;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\PlanPrice>
+ */
+class PlanPriceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected $model = PlanPrice::class;
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
+
+

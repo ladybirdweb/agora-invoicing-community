08b4ce4f80f46ca0c1f1d2aad179aeddc655c2d0
Sandesh Menath <sandesh.menath@ladybirdweb.com>
2023-05-28 12:50:02 +0530

    duplicate email job removal

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,13 +3,16 @@
 namespace App\Console;
 
 use App\Console\Commands\SetupTestEnv;
-use App\Jobs\CloudEmail;
 use App\Model\Common\StatusSetting;
 use App\Model\Mailjob\ActivityLogDay;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Illuminate\Support\Facades\Schema;
 use Torann\Currency\Console\Manage as CurrencyManage;
+use App\Model\Common\Setting;
+use App\Model\Mailjob\CloudEmail as cloudemailsend;
+use GuzzleHttp\Client;
+use Symfony\Component\Mime\Email;
 
 class Kernel extends ConsoleKernel
 {
@@ -43,10 +46,12 @@ class Kernel extends ConsoleKernel
         $this->execute($schedule, 'expiryMail');
         $this->execute($schedule, 'deleteLogs');
         $schedule->command('renewal:cron')
-                 ->daily();
+            ->daily();
         $this->execute($schedule, 'subsExpirymail');
         $this->execute($schedule, 'postExpirymail');
-        $schedule->job(new CloudEmail)->everyFiveMinutes();
+        $schedule->call(function () {
+            $this->cloudEmail();
+        })->everyFiveMinutes();
     }
 
     public function execute($schedule, $task)
@@ -135,5 +140,93 @@ class Kernel extends ConsoleKernel
     protected function commands()
     {
         require base_path('routes/console.php');
+    }
+
+
+
+
+
+    //This is to send an email to the client when the custom domain has been created properly
+    public function cloudEmail()
+    {
+
+        try {
+            $settings = Setting::find(1);
+            $mail = new \App\Http\Controllers\Common\PhpMailController();
+            $mailer = $mail->setMailConfig($settings);
+            $clouds = cloudemailsend::all();
+
+            foreach ($clouds as $cloud) {
+                if ($this->checkTheAvailabilityOfCustomDomain($cloud->domain, $cloud->counter, $cloud->user)) {
+                    $userData = $cloud->result_message.'.<br> Email:'.' '.$cloud->user.'<br>'.'Password:'.' '.$cloud->result_password;
+                    $email = (new Email())
+                        ->from($settings->email)
+                        ->to($cloud->user)
+                        ->subject('New instance created')
+                        ->html($cloud->result_message.'.<br> Email:'.' '.$cloud->user.'<br>'.'Password:'.' '.$cloud->result_password);
+
+                    $mailer->send($email);
+
+                    $mail->email_log_success($settings->email, $cloud->user, 'New instance created', $cloud->result_message.'.<br> Email:'.' '.$cloud->user.'<br>'.'Password:'.' '.$cloud->result_password);
+
+//                    $mail = new \App\Http\Controllers\Common\PhpMailController();
+//
+//                    $mail->sendEmail($settings->email, $cloud->user, $userData, 'New instance created');
+
+                    cloudemailsend::where('domain', $cloud->domain)->delete();
+                }
+            }
+        } catch(\Exception $e) {
+            $this->googleChat($e->getMessage());
+        }
+    }
+
+    private function checkTheAvailabilityOfCustomDomain($domain, $counter, $user)
+    {
+        $client = new Client();
+        try {
+            $response = $client->get('https://'.$domain);
+            $statusCode = $response->getStatusCode();
+            if ($statusCode >= 200 && $statusCode < 300) {
+                $this->prepareMessages($domain, $counter, $user, true);
+
+                return true;
+            }
+        } catch (\Exception $e) {
+            $this->prepareMessages($domain, $counter, $user);
+            // The domain is not reachable or the SSL certificate is invalid.
+            return false;
+        }
+        $this->prepareMessages($domain, $counter, $user);
+
+        return false;
+    }
+
+    private function googleChat($text)
+    {
+        $url = env('GOOGLE_CHAT');
+        $message = [
+            'text' => $text,
+        ];
+        $message_headers = [
+            'Content-Type' => 'application/json; charset=UTF-8',
+        ];
+        $client = new Client();
+        $client->post($url, [
+            'headers' => $message_headers,
+            'body' => json_encode($message),
+        ]);
+    }
+
+    private function prepareMessages($domain, $counter, $user, $success = false)
+    {
+        if ($success) {
+            $this->googleChat('Hello, It has come to my notice that this domain has been created successfully Domain name:'.$domain.' and this is their email: '.$user."\u{2705}\u{2705}\u{2705}");
+        } else {
+            cloudemailsend::where('domain', $domain)->increment('counter');
+            if ($counter == 30) {
+                $this->googleChat('Hello, It has come to my notice that this domain has not been created successfully Domain name:'.$domain.' and this is their email: '.$user.'&#10060;'."\u{2716}\u{2716}\u{2716}");
+            }
+        }
     }
 }

d09604fc122d15d0df9506b674d221484e93240b
sowmiyasubramaniyan <sowmiya.subramaniyan@ladybirdweb.com>
2023-06-13 11:56:39 +0200

    Added more changes

--- resources/views/themes/default1/front/demoForm.blade.php
+++ resources/views/themes/default1/front/demoForm.blade.php
@@ -1,70 +1,34 @@
-@extends('themes.default1.layouts.front.master')
-@section('title')
-Book For Demo
-@stop
-@section('page-header')
-Cart
-@stop
-@section('page-heading')
-Schedule Your Demo
-@stop
-@section('breadcrumb')
- @if(Auth::check())
-<li><a href="{{url('my-invoices')}}">Home</a></li>
-  @else
-  <li><a href="{{url('login')}}">Home</a></li>
-  @endif
-<li class="active">Request for demo</li>
-@stop
-@section('main-class') "main shop" @stop
-@section('content')   
-<style>
-    .required:after{ 
-        content:'*'; 
-        color:red; 
-        padding-left:5px;
-    }
-    .sub-text {
-    font-size: 1.25rem;
-    line-height: 1.875rem;
-    font-family: neue-haas-grotesk-text,Helvetica,sans-serif;
-    font-weight: 400;
-    font-style: normal;
-    color: #3b3f47;
-}
-</style>
-<?php 
-$set = new \App\Model\Common\Setting();
-$set = $set->findOrFail(1);
-?>
 
-<div class="row">
+        <div class="modal fade" id="demo-req" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
+             aria-hidden="true" style="margin-top: 15%;" >
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header text-center">
+                        <h4 class="modal-title w-100 font-weight-bold">Book For Demo</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                
 
-    <div class="col-md-6">
+                    <div class="card-body">
+      
+                        <div class="box-content">
 
-<h1 class="heading-primary" style="font-weight: bold;"><strong>Empower Your Support Teams with Faveo helpdesk: Book a Personalized Demo</strong></h1>
-<p class="sub-text">Our team will provide a live demonstration of Faveo helpdesk, focusing on the features and functionalities that align with your specific needs and challenges. We'll showcase how Faveo helpdesk can streamline your support processes and enhance your agents' productivity.</p>
-       <ul class="list list-icons list-icons-style-3 mt-4">
-                                <li><i class="fa fa-check" aria-hidden="true"></i>
-                               Fill out the Demo Request Form: Provide us with some basic details about your organization and your specific requirements. This information will help us tailor the demo to your needs effectively.</li>
-                                <li><i class="fa fa-check" aria-hidden="true"></i>
-                                 Schedule a Convenient Time: Choose a date and time that works best for you and your team. We offer flexible scheduling options to accommodate your availability.
-                                <li><i class="fa fa-check" aria-hidden="true"></i>
-                                Confirmation and Demo Details: Once you submit the request form, our team will review it and reach out to you to confirm the demo session. We'll provide you with all the necessary details, including the meeting link and instructions.</li>
-  
+                           
+                        {!! Form::open(['url'=>'demo-request','method' => 'post']) !!}
 
-    </div>
-        <div class="col-md-6">
+                            
 
-        {!! Form::open(['url'=>'postDemoReq']) !!}
-            <div class="form-row">
+                         
+                              <div class="form-row">
                 <div class="form-group col-lg-6">
                   
-                        <label class="required">Your name</label>
+                        <label class="required">Name</label>
                         <input type="text" value="" data-msg-required="Please enter your name." maxlength="100" class="form-control" name="name" id="name" required>
                     </div>
                    <div class="form-group col-lg-6">
-                        <label class="required">Your email address</label>
+                        <label class="required">Email address</label>
                         <input type="email" value="" data-msg-required="Please enter your email address." data-msg-email="Please enter a valid email address." maxlength="100" class="form-control" name="email" id="email" required>
                     </div>
                 </div>
@@ -92,17 +56,22 @@ $set = $set->findOrFail(1);
             </div>
               <div class="form-row">
                 <div class="form-group col">
-                    <input type="submit" style="width: 100%;" value="Book For Demo" class="btn btn-primary btn-lg mb-xlg" data-loading-text="Loading...">
+                    <input type="submit" style="width: 100%;" value="Book a Demo" class="btn btn-primary btn-lg mb-xlg" data-loading-text="Loading...">
                 </div>
             </div>
-            <p style="text-align: center;font-size: 17px;"><a style="color: green;font-weight: bold !important;" href="{{url('login')}}">Or sign up for a 15-day free trial</a></p>
-        {!! Form::close() !!}
-    </div>
+        
 
-</div>
-@stop
-@section('script')
-<script type="text/javascript">
+          <p style="text-align: center;font-size: 17px;"><a style="color: green;font-weight: bold !important;" href="{{url('login')}}">Or sign up for a {{$days}}-day free trial</a></p>
+                            {!! Form::close() !!}
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+        
+        <script type="text/javascript">
     
     
             var telInput = $('#mobilenum'),
@@ -179,4 +148,3 @@ $set = $set->findOrFail(1);
         });
 
 </script>
-@stop

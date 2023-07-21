6d846124b65b377fa17bcf1b9988dbd5d0ad4979
sowmiyasubramaniyan <sowmiya.subramaniyan@ladybirdweb.com>
2023-07-21 14:29:58 +0530

    code corrections

--- resources/views/themes/default1/user/client/show.blade.php
+++ resources/views/themes/default1/user/client/show.blade.php
@@ -374,11 +374,11 @@ User
                                             <b>Referrer</b>: <span class="pull-right referrer" style="float:right;"></span>
                                         </li>
                                         <li class="list-group-item">
-                                        <b>Number of Email Verification Attempts:</b>
+                                         <b>{{Lang::get('message.mail_verify')}}</b>: 
                                         <span class="pull-right mailVerify" style="float:right;">{{ $email }}</span>
                                     </li>
                                     <li class="list-group-item">
-                                        <b>Number of Mobile Verification Attempts:</b>
+                                          <b>{{Lang::get('message.mobile_verify')}}</b>: 
                                         <span class="pull-right mobileVerify" style="float:right;">{{$mobile}}</span>
                                     </li>
 

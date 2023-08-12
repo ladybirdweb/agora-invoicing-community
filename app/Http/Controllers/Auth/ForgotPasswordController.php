3381f073b332dda674461e5aad4ddaac4ae4ce07
sowmiyasubramaniyan <sowmiya.subramaniyan@ladybirdweb.com>
2023-08-12 18:43:14 +0200

    issue fixed

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Model\Common\TemplateType;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 use Illuminate\Http\Request;
-
+use App\Model\Common\TemplateType;
 class ForgotPasswordController extends Controller
 {
     /*
@@ -89,8 +89,9 @@ class ForgotPasswordController extends Controller
                 $type = $temp_type->where('id', $type_id)->first()->name;
             }
 
-            $mail = new \App\Http\Controllers\Common\PhpMailController();
+            
             if (emailSendingStatus()) {
+                $mail = new \App\Http\Controllers\Common\PhpMailController();
                 $mail->SendEmail($setting->email, $user->email, $template->data, $template->name, $replace, $type);
                 $response = ['type' => 'success',   'message' =>'Reset instructions have been mailed to '.$user->email.'
     .Be sure to check your Junk folder if you do not see an email from us in your Inbox within a few minutes.'];

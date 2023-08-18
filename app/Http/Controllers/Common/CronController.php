6dbbaf38f458c2be2a55a9e0e2e9ecbf3ef00a34
Sowmiya Subramanian <96425427+sowmiyasubramaniyan@users.noreply.github.com>
2023-08-18 19:18:11 +0530

    changes

--- app/Http/Controllers/Common/CronController.php
+++ app/Http/Controllers/Common/CronController.php
@@ -284,6 +284,18 @@ class CronController extends BaseCronController
 
         return $subscriptions;
     }
+        public function getautoSubscriptions($days)
+    {
+        $daysArray = $days;
+        $days = (int) $daysArray[0];
+        $days = intval($daysArray[0]);
+
+        $startDate = Carbon::now()->toDateString();
+
+        $endDate = Carbon::now()->addDays($days + 1)->toDateString();
+        $subscriptions = Subscription::whereBetween('update_ends_at', [$startDate, $endDate])->where('is_subscribed', '1')->get();
+        return $subscriptions;
+    }
 
     public function eachSubscription()
     {
@@ -311,9 +323,8 @@ class CronController extends BaseCronController
         $status = StatusSetting::value('subs_expirymail');
         if ($status == 1) {
             $Days = ExpiryMailDay::pluck('autorenewal_days')->toArray();
-
             $cron = new AutorenewalCronController();
-            $Autosub = $cron->getAutoSubscriptions($Days);
+            $Autosub = $this->getautoSubscriptions($Days);
             foreach ($Autosub as $value) {
                 $userid = $value->user_id;
                 $user = $this->getUserById($userid);
@@ -347,8 +358,12 @@ class CronController extends BaseCronController
                 if (emailSendingStatus()) {
                     $this->Expiredsub_Mail($user, $end, $product, $order, $value->id);
                 }
+           
             }
         }
+      
+        
+        
     }
 
     public function getOnDayExpiryInfoSubs()

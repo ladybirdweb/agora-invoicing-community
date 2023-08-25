e7bbe0bc62c8373369e2c0dab89e8a99f66a788f
Sandesh Menath <sandesh.menath@ladybirdweb.com>
2023-08-25 16:47:47 +0530

    messages

--- app/Http/Controllers/Front/CheckoutController.php
+++ app/Http/Controllers/Front/CheckoutController.php
@@ -218,7 +218,6 @@ class CheckoutController extends InfoController
 
             return view('themes.default1.front.paynow', compact('invoice', 'items', 'product', 'paid'));
         } catch (\Exception $ex) {
-            dd($ex);
             app('log')->error($ex->getMessage());
 
             return redirect()->back()->with('fails', $ex->getMessage());

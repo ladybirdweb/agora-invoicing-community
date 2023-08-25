230488c9e29883fd9db35395d4a4730f09914513
Gurmeenkour99 <gurmeen.kour@gmail.com>
2023-08-25 10:53:48 +0530

    remove comment

--- app/Http/Controllers/Order/BaseOrderController.php
+++ app/Http/Controllers/Order/BaseOrderController.php
@@ -132,7 +132,7 @@ class BaseOrderController extends ExtendedOrderController
 
     protected function updateInvoiceDate($invoiceId)
     {
-        $invoice = Invoice::find($invoiceId); // Replace Invoice with your actual Invoice model
+        $invoice = Invoice::find($invoiceId); 
         $invoice->update(['date' => Carbon::now()]);
     }
 

5a908d9de2ca08183fd78c044ea3e80677b9d36c
sowmiyasubramaniyan <sowmiya.subramaniyan@ladybirdweb.com>
2023-08-25 12:18:40 +0530

    added null in coupon code and icon issue fixed

---
+++ database/migrations/2023_08_25_064546_make_coupon_code_nullable_in_invoices.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('invoices', function (Blueprint $table) {
+            $table->string('coupon_code')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('invoices', function (Blueprint $table) {
+            $table->string('coupon_code')->change();
+
+        });
+    }
+};

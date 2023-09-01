be0548cb4a1abb79033417edda25324b51dea9dc
Sandesh Menath <sandesh.menath@ladybirdweb.com>
2023-09-01 10:16:38 +0530

    models

--- app/Http/Controllers/RazorpayController.php
+++ app/Http/Controllers/RazorpayController.php
@@ -8,6 +8,7 @@ use App\Model\Common\State;
 use App\Model\Order\Invoice;
 use App\Model\Order\InvoiceItem;
 use App\Model\Order\Order;
+use App\Model\Order\Payment;
 use App\Model\Payment\TaxByState;
 use App\Model\Product\Product;
 use App\Plugins\Stripe\Controllers\SettingsController;
@@ -182,10 +183,10 @@ class RazorpayController extends Controller
 
     private function doTheDeed($invoice)
     {
-        $amt_to_credit = \DB::table('payments')->where('user_id', \Auth::user()->id)->where('payment_status', 'success')->where('payment_method', 'Credit Balance')->value('amt_to_credit');
+        $amt_to_credit = Payment::where('user_id', \Auth::user()->id)->where('payment_status', 'success')->where('payment_method', 'Credit Balance')->value('amt_to_credit');
         if ($amt_to_credit) {
             $amt_to_credit = $amt_to_credit - $invoice->billing_pay;
-            \DB::table('payments')->where('user_id', \Auth::user()->id)->where('payment_method', 'Credit Balance')->where('payment_status', 'success')->update(['amt_to_credit'=>$amt_to_credit]);
+            Payment::where('user_id', \Auth::user()->id)->where('payment_method', 'Credit Balance')->where('payment_status', 'success')->update(['amt_to_credit'=>$amt_to_credit]);
             User::where('id', \Auth::user()->id)->update(['billing_pay_balance'=>0]);
             $payment_id = \DB::table('payments')->where('user_id', \Auth::user()->id)->where('payment_status', 'success')->where('payment_method', 'Credit Balance')->value('id');
             $formattedValue = currencyFormat($invoice->billing_pay, $invoice->currency, true);

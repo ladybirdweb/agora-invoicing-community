a21de035019442d129649a57ee1d9922abd3e62f
Sandesh Menath <sandesh.menath@ladybirdweb.com>
2023-09-12 12:13:27 +0530

    new changes

--- app/Http/Controllers/FreeTrailController.php
+++ app/Http/Controllers/FreeTrailController.php
@@ -232,6 +232,7 @@ class FreeTrailController extends Controller
             $this->orderNo = $order->number;
             $baseorder = new BaseOrderController();
             $baseorder->addOrderInvoiceRelation($invoiceid, $order->id);
+            \Session::put('planDays','freeTrial');
 
             if ($plan_id) {
                 $baseorder->addSubscription($order->id, $plan_id, $version, $product, $serial_key);
@@ -240,7 +241,7 @@ class FreeTrailController extends Controller
             if ($mailchimpStatus) {
                 $baseorder->addtoMailchimp($product, $user_id, $item);
             }
-
+            \Session::forget('planDays');
             return $serial_key;
         } catch (\Exception $ex) {
             app('log')->error($ex->getMessage());

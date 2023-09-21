4e0b11b48325a2c82c513c1a6635fe9b4de43fe8
Gurmeenkour99 <gurmeen.kour@ladybirdweb.com>
2023-09-21 11:35:48 +0530

    orderLinking with license

--- app/Http/Controllers/LicenseBillOrders.php
+++ app/Http/Controllers/LicenseBillOrders.php
@@ -1,15 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Model\Order\Order;
 
+use App\Model\Order\Order;
 use Illuminate\Http\Request;
+use App\ThirdPartyApp;
 
 class LicenseBillOrders extends Controller
 {
-    public function orderid($number){
-        $data= Order::where('number', $number)->value('id');
-        return redirect("orders/".$data);
+    public function orderid(Request $request ,$number)
+    {
+         $app = ThirdPartyApp::where('app_secret', $request->secret)
+        ->first();
+        if($request->secret == $app->app_secret){
+             $data = Order::where('number', $number)->value('id');
+        return $data;            
+        }        
     }
-    }
-
+}

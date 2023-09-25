8be8b1bd66f5d6cc4487b753a1ac514341c20c2f
sowmiyasubramaniyan <sowmiya.subramaniyan@ladybirdweb.com>
2023-09-25 09:12:21 +0200

    migration changes

---
+++ database/migrations/2023_09_25_061547_rename_columns_in_verification_attempts.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('verification_attempts', function (Blueprint $table) {
+             $table->renameColumn('type', 'mobile_attempt')->nullabe();
+            $table->renameColumn('attempt_count', 'email_attempt')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('verification_attempts', function (Blueprint $table) {
+            $table->renameColumn('mobile_attempt', 'type');
+            $table->renameColumn('email_attempt', 'attempt_count');
+        });
+    }
+};

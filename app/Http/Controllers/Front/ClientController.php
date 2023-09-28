10aee412b36394a8cb908f4dca54e4cdb0077048
Sandesh Menath <sandesh.menath@ladybirdweb.com>
2023-09-28 14:24:26 +0530

    client negative

--- app/Http/Controllers/Front/ClientController.php
+++ app/Http/Controllers/Front/ClientController.php
@@ -282,6 +282,10 @@ class ClientController extends BaseClientController
                          }
                          $pendingAmount = $model->grand_total - $sum;
 
+                         if($pendingAmount<0){
+                             $pendingAmount=0;
+                         }
+
                          return currencyFormat($pendingAmount, $code = $model->currency);
                      })
                      ->addColumn('status', function ($model) {

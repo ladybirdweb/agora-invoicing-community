07188bca1f050dca6344a0f45cfe9a99de752241
sowmiyasubramaniyan <sowmiya.subramaniyan@ladybirdweb.com>
2023-10-16 11:18:07 +0530

    Template replyto changes

---
+++ config/shortcodes.php
@@ -0,0 +1,35 @@
+<?php
+
+
+return [
+    '{{website_url}}' => 'The URL of the website',
+    '{{name}}' => 'User\'s name',
+    '{{username}}' => 'User\'s username',
+    '{{password}}' => 'User\'s password',
+    '{{url}}' => 'URL of the resource',
+    '{{logo}}' => 'Logo URL',
+    '{{contact}}' => 'Contact information',
+    '{{reply_email}}' => 'Email address for replies',
+    '{{downloadurl}}' => 'Download URL',
+    '{{invoiceurl}}' => 'Invoice URL',
+    '{{product}}' => 'Product information',
+    '{{number}}' => 'Product number',
+    '{{expiry}}' => 'Expiry date',
+    '{{serialkeyurl}}' => 'Serial key URL',
+    '{{knowledge_base}}' => 'Knowledge base URL',
+    '{{orderHeading}}' => 'Order heading information',
+    '{{deletionDate}}' => 'Deletion date information',
+    '{{manager_first_name}}' => 'Manager\'s first name',
+    '{{manager_last_name}}' => 'Manager\'s last name',
+    '{{manager_email}}' => 'Manager\'s email',
+    '{{manager_code}}' => 'Manager\'s code',
+    '{{manager_mobile}}' => 'Manager\'s mobile number',
+    '{{manager_skype}}' => 'Manager\'s Skype ID',
+    '{{future_expiry}}' => 'Future expiry information',
+    '{{message}}' => 'Message information',
+    '{{title}}' => 'Title information',
+    '{{company_email}}' => 'Company email address',
+];
+
+
+?>

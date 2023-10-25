df21a20be66af44aaa4a9490c717a4020c1f562d
sowmiyasubramaniyan <sowmiya.subramaniyan@ladybirdweb.com>
2023-10-25 15:18:01 +0530

    css and js files added

---
+++ public/client/porto/js-2/demo-transportation-logistic.js
@@ -0,0 +1,13 @@
+/*
+Name:           Demo Transportation Logistic
+Written by:     Okler Themes - (http://www.okler.net)
+Theme Version:  9.9.0
+*/
+
+(function( $ ) {
+	
+	'use strict';
+
+	
+
+}).apply( this, [ jQuery ]);

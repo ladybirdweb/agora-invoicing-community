523fcc1b54dd04de0b66c8665f537055b37d963e
Gurmeenkour99 <gurmeen.kour@gmail.com>
2023-10-30 11:48:35 +0530

    address changes

--- resources/views/themes/default1/front/contact.blade.php
+++ resources/views/themes/default1/front/contact.blade.php
@@ -98,7 +98,7 @@ $set = $set->findOrFail(1);
 
         <h4 class="heading-primary">Our Office</h4>
        <ul class="list list-icons list-icons-style-3 mt-4">
-                                <li><i class="fas fa-map-marker-alt"></i> <strong>Address:</strong> {{$set->address}},{{$set->city}},{{$state}},{{$country}},{{$set->zip}}.</li>
+                                <li><i class="fas fa-map-marker-alt"></i> <strong>Address:</strong> {{$set->address}},&nbsp;{{$set->city}},&nbsp;{{$state}},&nbsp;{{$country}},&nbsp;{{$set->zip}}.</li>
                                 <li><i class="fas fa-phone"></i> <strong>Phone: </strong><b>+</b>{{$set->phone_code}} {{$set->phone}}</li>
                                 <li><i class="far fa-envelope"></i> <strong>Email:</strong> <a href="mailto:{{$set->company_email}}">{{$set->company_email}}</a></li>
                             </ul>

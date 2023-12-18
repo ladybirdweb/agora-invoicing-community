89e55782c0d78ea7649062494f466b0c8f44e1d3
Gurmeenkour99 <gurmeen.kour@gmail.com>
2023-12-18 15:22:48 +0530

    twitter

--- app/Http/Controllers/Front/WidgetController.php
+++ app/Http/Controllers/Front/WidgetController.php
@@ -143,7 +143,9 @@ class WidgetController extends Controller
                 throw new \Exception('Allow Social icon  can be selected as Yes only for one of the footers. It has alrerady been selected for a footer. Please change it to No to activate social icon for this footer.');
             }
             $widget = $this->widget->where('id', $id)->first();
-            $widget->fill($request->input())->save();
+            $widget->fill($request->input());
+            $widget->allow_tweets = 0;
+            $widget->save();
 
             return redirect()->back()->with('success', \Lang::get('message.updated-successfully'));
         } catch (\Exception $ex) {

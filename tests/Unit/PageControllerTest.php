d46dc1dafcbc9e3f6dcc97734b8ccc91b5824b56
Sowmiya Subramanian <96425427+sowmiyasubramaniyan@users.noreply.github.com>
2024-02-01 11:52:50 +0530

    Added test cases

--- tests/Unit/PageControllerTest.php
+++ tests/Unit/PageControllerTest.php
@@ -7,10 +7,18 @@ use App\Model\Front\FrontendPage;
 use App\Model\Payment\Plan;
 use App\Model\Product\Product;
 use App\User;
-use Tests\TestCase;
+use Tests\DBTestCase;
 
-class PageControllerTest extends TestCase
+class PageControllerTest extends DBTestCase
 {
+
+    private $con;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->con = new PageController();
+    }
     /**
      * A basic unit test example.
      *
@@ -92,4 +100,43 @@ class PageControllerTest extends TestCase
         $expectedPrice = '12  $1,080.00 per year';
         $this->assertEquals($expectedPrice, $result[$value->id]);
     }
+
+    public function testDetectSpamWithExcessivePunctuation()
+    {
+        $message = 'This is a spam message with too many exclamation marks!!!!!!!!!!';
+        $response = $this->getPrivateMethod($this->con , 'containsExcessivePunctuation',[$message]);
+        $this->assertTrue($response);
+    }
+
+    public function testDetectSpamWithExcessiveCaps()
+    {
+        $message = 'This is a SPAM message with TOO MANY CAPITAL LETTERS';
+        $response = $this->getPrivateMethod($this->con , 'containsExcessiveCaps',[$message]);
+        $this->assertTrue($response);
+    }
+
+    public function testDetectSpamWithSpamKeywords()
+    {
+        $message = 'This message contains the word Viagra';
+        $response = $this->getPrivateMethod($this->con , 'containsSpamKeywords',[$message]);
+        $this->assertTrue($response);
+    }
+
+    public function testDetectNonSpam()
+    {
+        $email = "example@gmail.com";
+        $message = 'Hi,Need a demo.';
+        $response = $this->getPrivateMethod($this->con , 'detectSpam',[$email,$message]);
+        $this->assertFalse($response);
+    }
+
+    public function testDetectSpam()
+    {
+        $email = "example@gmail.com";
+        $message = 'Promo codes and promotions are a great way to get additional benefits from using the online bookmaker 1xbet. Promo codes for one x bet One of these promo codes is 1xpromo. It makes it possible to receive an additional bonus to your account when registering on the bookmaker’s website. In order to take advantage of this offer, you need to enter a promo code during registration and make the first deposit of at least 1000 rubles. After this, a bonus in the amount of 100% of the deposit amount will be credited to your account. The received bonus can be used for betting on sporting events or online casinos. The conditions for receiving and using the bonus are described in detail on the official 1xbet website. The site also regularly holds promotions, within which you can receive additional promotional codes or bonuses. Follow the news and don’t miss the opportunity to get even more benefits from playing at 1xbet.';
+        $response = $this->getPrivateMethod($this->con , 'detectSpam',[$email,$message]);
+        $this->assertTrue($response);
+    }
+
+
 }

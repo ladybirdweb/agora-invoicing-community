dacc8e7622363a5b2d86855d4337709d71d19d19
Sowmiya Subramanian <96425427+sowmiyasubramaniyan@users.noreply.github.com>
2024-02-07 10:41:51 +0530

    Update shoppingcart.blade.php

--- resources/views/themes/default1/common/template/shoppingcart.blade.php
+++ resources/views/themes/default1/common/template/shoppingcart.blade.php
@@ -236,8 +236,7 @@ main
                     <div class="col">
                         <div class="owl-carousel nav-outside nav-arrows-1 custom-carousel-box-shadow-2 appear-animation" data-appear-animation="fadeInUpShorter" data-appear-animation-delay="750" data-plugin-options="{'responsive': {'0': {'items': 1}, '479': {'items': 1}, '768': {'items': 2}, '979': {'items': 3}, '1199': {'items': 3}}, 'autoplay': false, 'autoplayTimeout': 5000, 'autoplayHoverPause': true, 'dots': false, 'nav': true, 'loop': false, 'margin': 20, 'stagePadding': '75'}">
                              {!! html_entity_decode($templates) !!}
-                            }
-                            }
+                           
 
                           </div>
                       </div>

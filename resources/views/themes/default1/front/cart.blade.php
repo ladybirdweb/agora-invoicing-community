737d5ca2788919c3adefa105eb68e6bf82d3b1e2
Sowmiya Subramanian <96425427+sowmiyasubramaniyan@users.noreply.github.com>
2024-04-27 11:53:54 +0530

    blink issue fixed

--- resources/views/themes/default1/front/cart.blade.php
+++ resources/views/themes/default1/front/cart.blade.php
@@ -144,9 +144,9 @@ Cart
                                                         <i class="fas fa-times"></i>
                                                     </a>
 
-                                                    <span class="product-thumbnail-image"  data-bs-toggle="tooltip" title="{{$item->name}}">
+                                                    <span class="product-thumbnail-image" >
 
-                                                        <img width="90" height="90" alt="" class="img-fluid" src="{{$item->associatedModel->image}}">
+                                                        <img width="90" height="90" alt="" class="img-fluid" src="{{$item->associatedModel->image}}"  data-bs-toggle="tooltip" title="{{$item->name}}">
                                                     </span>
                                                 </div>
                                             </td>

4927efac924040af6f09f641f1658179fdef4f16
sowmiyasubramaniyan <sowmiya.subramaniyan@ladybirdweb.com>
2024-06-13 16:22:17 +0530

    error log and slowness issue in store page

--- app/Http/Controllers/User/ClientController.php
+++ app/Http/Controllers/User/ClientController.php
@@ -297,10 +297,15 @@ class ClientController extends AdvanceSearchController
             $extraAmt = $this->getExtraAmt($id);
             $client = $this->user->where('id', $id)->first();
 
+            if (is_null($client)) {
+            throw new \Exception('User not found.');
+            }
+
             if (User::onlyTrashed()->find($id)) {
                 $client = User::onlyTrashed()->find($id);
             }
 
+           
             $is2faEnabled = $client->is_2fa_enabled;
             $currency = getCurrencyForClient($client->country);
             $orders = $order->where('client', $id)->get();

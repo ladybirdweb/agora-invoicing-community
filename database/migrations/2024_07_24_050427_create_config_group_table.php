78104ff75775eb4a8e1074a697a4584f25ee38a5
Sandesh Menath <sandesh.menath@ladybirdweb.com>
2024-07-29 13:05:08 +0530

    some more changes

---
+++ database/migrations/2024_07_24_050427_create_config_group_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('config_group', function (Blueprint $table) {
+
+            $table->id();
+            $table->string('config_group_name', 255);
+            $table->binary('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('config_group');
+    }
+};

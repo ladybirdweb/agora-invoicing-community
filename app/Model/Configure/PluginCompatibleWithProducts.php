53dcd80e3ef6712f0de9cf8c1e8155806f9a8813
Sandesh Menath <sandesh.menath@ladybirdweb.com>
2024-07-31 11:40:15 +0530

    Plugin compatible for purchase

---
+++ app/Model/Configure/PluginCompatibleWithProducts.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Model\Configure;
+
+use App\Model\Product\Product;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PluginCompatibleWithProducts extends Model
+{
+    use HasFactory;
+
+    protected $table = 'plugin_compatible_with_products';
+
+    protected $guarded = [];
+
+    // Define the relationship with Product (as product)
+    public function productComp()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
+    // Define the relationship with Product (as plugin)
+    public function pluginComp()
+    {
+        return $this->belongsTo(Product::class, 'plugin_id');
+
+    }
+}

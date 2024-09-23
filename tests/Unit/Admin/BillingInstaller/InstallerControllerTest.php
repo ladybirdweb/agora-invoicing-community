233a0dcae0d4495e538a99995b34ae32c8c50949
sadhasivam <sadhasivam.sekar@ladybirdweb.com>
2024-09-23 17:03:59 +0530

    testcase

--- tests/Unit/Admin/BillingInstaller/InstallerControllerTest.php
+++ tests/Unit/Admin/BillingInstaller/InstallerControllerTest.php
@@ -3,15 +3,18 @@
 namespace Tests\Unit\Admin\BillingInstaller;
 
 use App\Http\Controllers\BillingInstaller\InstallerController;
+use Artisan;
 use Illuminate\Http\Request;
 use Illuminate\Testing\TestResponse;
+use Session;
 use Tests\TestCase;
+use Mockery;
 
 class InstallerControllerTest extends TestCase
 {
     public function testConfigurationCheck()
     {
-//        This will only test the wrong connection because we dont know the database credential
+        // This will only test the wrong connection because we don't know the database credential
         $request = Request::create('/configurationcheck', 'POST', [
             'host' => 'localhost',
             'databasename' => 'test_db',
@@ -25,5 +28,48 @@ class InstallerControllerTest extends TestCase
         $response = TestResponse::fromBaseResponse($response); // Wrap the base response
         $response->assertStatus(200);
         $this->assertEquals(false, json_decode($response->getContent())->mysqli_ok);
+    }
+
+    public function test_checkPreInstall()
+    {
+        // Mock Artisan call
+        Artisan::shouldReceive('call')
+            ->once()
+            ->with('key:generate', ['--force' => true]);
+
+        $controller = new InstallerController();
+        $response = $controller->checkPreInstall();
+
+        $this->assertEquals(200, $response->status());
+        $data = json_decode($response->getContent(), true);
+        $this->assertEquals('Pre migration has been tested successfully', $data['result']['success']);
+        $this->assertEquals('Migrating tables in database', $data['result']['next']);
+    }
+    public function test_createEnv(){
+        // Arrange
+        Session::shouldReceive('put')
+            ->with('default', 'mysql');
+        // Mocking session, cache, etc.
+
+        // Mocking InstallerController and allowing to mock protected methods
+        $controller = Mockery::mock('App\Http\Controllers\InstallerController')
+            ->shouldAllowMockingProtectedMethods();
+
+        // Mock the protected method 'createEnv'
+        $controller->shouldReceive('createEnv')
+            ->with(true)
+            ->andReturn(response()->json([
+                'result' => [
+                    'success' => 'Environment configuration file has been created successfully'
+                ]
+            ], 200));
+
+        // Act
+        $response = $controller->createEnv(true);
+
+        // Assert
+        $this->assertEquals(200, $response->status());
+        $data = json_decode($response->getContent(), true);
+        $this->assertEquals('Environment configuration file has been created successfully', $data['result']['success']);
     }
 }

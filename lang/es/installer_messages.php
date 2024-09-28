95e2e40e4b48a46108a185de11b057d29495ca8f
sadhasivam <sadhasivam.sekar@ladybirdweb.com>
2024-09-28 12:26:21 +0530

    add lang for all languages

--- lang/es/installer_messages.php
+++ lang/es/installer_messages.php
@@ -2,63 +2,110 @@
 
 return [
 
-    /*
-     *
-     * Traducciones compartidas.
-     *
-     */
-    'title' => 'Instalador de Laravel',
-    'next' => 'Siguiente',
-    'finish' => 'Instalar',
+    'title' => 'Instalador de Agora Facturación',
+    'probe' => 'Pruebas de Agora Facturación',
+    'magic_phrase' => '¿Cuál es la frase mágica?',
+    'server_requirements' => 'Requisitos del Servidor',
+    'database_setup'=> 'Configuración de la Base de Datos',
+    'getting_started' => 'Comenzando',
+    'final' => 'Final',
+    'directory' => 'Directorio',
+    'permissions' => 'Permisos',
+    'requisites' => 'Requisitos',
+    'status' => 'Estado',
+    'php_extensions' => 'Extensiones de PHP',
+    'not_enabled' => 'No habilitado',
+    'extension_not_enabled' => 'No habilitado: Para habilitar esto, instale la extensión en su servidor y actualice :php_ini_file para habilitar :extensionName. <a href=":url" target="_blank">¿Cómo instalar extensiones de PHP en mi servidor?</a>',
+    'mod_rewrite' => 'Mod Rewrite',
+    'off_apache' => 'APAGADO (Si está usando Apache, asegúrese de que <var><strong>AllowOverride</strong></var> esté configurado como <var><strong>All</strong></var> en la configuración de Apache)',
+    'rewrite_engine' => 'Motor de Reescritura',
+    'user_url' => 'URL amigable para el usuario',
 
-    /*
-     *
-     * Traducciones de la página principal.
-     *
-     */
-    'welcome' => [
-        'title' => 'Bienvenido al instalador',
-        'message' => 'Bienvenido al asistente de configuración',
+    'host' => 'Host',
+    'host_tooltip' => 'Si su MySQL está instalado en el mismo servidor que Agora Facturación, déjelo como localhost',
+    'database_name_label' => 'Nombre de la base de datos',
+    'mysql_port_label' => 'Número de puerto de MySQL',
+    'mysql_port_tooltip' => 'Número de puerto en el que su servidor MySQL está escuchando. Por defecto, es 3306',
+    'username' => 'Nombre de usuario',
+    'password_label' => 'Contraseña',
+    'test_prerequisites_message' => 'Esta prueba verificará los requisitos necesarios para instalar Agora Facturación',
+    'previous' => 'Anterior',
+
+    'sign_up_as_admin' => 'Registrarse como Administrador',
+    'first_name' => 'Nombre',
+    'first_name_required' => 'El nombre es requerido',
+    'last_name' => 'Apellido',
+    'last_name_required' => 'El apellido es requerido',
+    'username_info' => 'El nombre de usuario puede contener solo caracteres alfanuméricos, espacios, guiones bajos, guiones, puntos y el símbolo @.',
+    'email' => 'Correo electrónico',
+    'email_required' => 'El correo electrónico del usuario es requerido',
+    'password_required' => 'La contraseña es requerida',
+    'confirm_password' => 'Confirmar contraseña',
+    'confirm_password_required' => 'La confirmación de contraseña es requerida',
+    'password_requirements' => 'Tu contraseña debe tener:',
+    'password_requirements_list' => [
+        'Entre 8-16 caracteres',
+        'Letras mayúsculas (A-Z)',
+        'Letras minúsculas (a-z)',
+        'Números (0-9)',
+        'Caracteres especiales (~*!@$#%_+.?:,{ })',
     ],
 
-    /*
-     *
-     * Tranducciones de la página de requisitos.
-     *
-     */
-    'requirements' => [
-        'title' => 'Requisitos',
-    ],
+    // System Information
+    'system_information' => 'Información del Sistema',
+    'environment' => 'Entorno',
+    'environment_required' => 'El entorno es requerido',
+    'production' => 'Producción',
+    'development' => 'Desarrollo',
+    'testing' => 'Pruebas',
+    'cache_driver' => 'Controlador de Caché',
+    'cache_driver_required' => 'El controlador de caché es requerido',
+    'file' => 'Archivo',
+    'redis' => 'Redis',
+    'password' => 'Contraseña',
 
-    /*
-     *
-     * Traducciones de la pagina de permisos.
-     *
-     */
-    'permissions' => [
-        'title' => 'Permisos',
-    ],
+    // Redis Setup
+    'redis_setup' => 'Configuración de Redis',
+    'redis_host' => 'Host de Redis',
+    'redis_port' => 'Puerto de Redis',
+    'redis_password' => 'Contraseña de Redis',
 
-    /*
-     *
-     * Traducciones de la página de entorno.
-     *
-     */
-    'environment' => [
-        'title' => 'Configuraciones del entorno',
-        'save' => 'Guardar archivo .env',
-        'success' => 'Los cambios en tu archivo .env han sido guardados.',
-        'errors' => 'No es posible crear el archivo .env, por favor intentalo manualmente.',
-    ],
+    // Buttons
+    'continue' => 'Continuar',
 
-    /*
-     *
-     * Traducciones de la página final.
-     *
-     */
-    'final' => [
-        'title' => 'Finalizado.',
-        'finished' => 'La aplicación ha sido instalada con éxito!',
-        'exit' => 'Haz click aquí para salir.',
-    ],
+    // Final Setup
+    'final_setup' => '¡Tu Aplicación de Agora Facturación está lista!',
+    'installation_complete' => '¡Todo listo, Sparky! Has completado la instalación.',
+
+    // Learn More
+    'learn_more' => 'Aprender más',
+    'knowledge_base' => 'Base de conocimientos',
+    'email_support' => 'Soporte por correo electrónico',
+
+    // Next Step
+    'next_step' => 'Siguiente Paso',
+    'login_button' => 'Iniciar sesión en Facturación',
+
+    'pre_migration_success' => 'La pre-migración se ha probado con éxito',
+    'migrating_tables' => 'Migrando tablas en la base de datos',
+    'db_connection_error' => 'La conexión a la base de datos no se actualizó.',
+    'database_setup_success' => 'La base de datos se ha configurado correctamente.',
+    'env_file_created' => 'El archivo de configuración del entorno se ha creado correctamente',
+    'pre_migration_test' => 'Ejecutando prueba de pre-migración',
+
+    'redis_host_required' => 'El host de Redis es requerido.',
+    'redis_password_required' => 'La contraseña de Redis es requerida.',
+    'redis_port_required' => 'El puerto de Redis es requerido.',
+    'password_regex' => 'La contraseña debe contener al menos 8 caracteres, una letra mayúscula, una letra minúscula, un número y un carácter especial.',
+    'setup_completed' => '¡Configuración completada con éxito!',
+
+    'database' => 'Base de datos',
+    'selected' => 'Seleccionado',
+    'mysql_version_is' => 'La versión de MySQL es',
+    'database_empty' => 'La base de datos está vacía',
+    'database_not_empty' => 'La instalación de Agora Facturación requiere una base de datos vacía, su base de datos ya tiene tablas y datos.',
+    'mysql_version_required' => '¡Recomendamos actualizar a al menos MySQL 5.6 o MariaDB 10.3!',
+    'database_connection_unsuccessful' => 'La conexión a la base de datos no fue exitosa.',
+    'connected_as' => 'Conectado a la base de datos como',
+    'failed_connection' => 'Error al conectar a la base de datos.',
 ];
--- lang/ru/installer_messages.php
+++ lang/ru/installer_messages.php
@@ -2,62 +2,111 @@
 
 return [
 
-    /*
-     *
-     * Shared translations.
-     *
-     */
-    'title' => 'Установка Laravel',
-    'next' => 'Следующий шаг',
+    'title' => 'Установщик Agora Invoicing',
+    'probe' => 'Пробники Agora Invoicing',
+    'magic_phrase' => 'Какова волшебная фраза',
+    'server_requirements' => 'Требования к серверу',
+    'database_setup' => 'Настройка базы данных',
+    'getting_started' => 'Начало работы',
+    'final' => 'Завершение',
+    'directory' => 'Каталог',
+    'permissions' => 'Разрешения',
+    'requisites' => 'Требования',
+    'status' => 'Статус',
+    'php_extensions' => 'PHP расширения',
+    'not_enabled' => 'Не включено',
+    'extension_not_enabled' => 'Не включено: Чтобы включить это, пожалуйста, установите расширение на вашем сервере и обновите :php_ini_file для активации :extensionName. <a href=":url" target="_blank">Как установить PHP расширения на моем сервере?</a>',
+    'mod_rewrite' => 'Mod Rewrite',
+    'off_apache' => 'ВЫКЛ (Если вы используете Apache, убедитесь, что <var><strong>AllowOverride</strong></var> установлен на <var><strong>All</strong></var> в конфигурации Apache)',
+    'rewrite_engine' => 'Модуль перезаписи',
+    'user_url' => 'Дружественный URL',
 
-    /*
-     *
-     * Home page translations.
-     *
-     */
-    'welcome' => [
-        'title' => 'Установка Laravel',
-        'message' => 'Добро пожаловать в первоначальную настройку фреймворка Laravel.',
+    'host' => 'Хост',
+    'host_tooltip' => 'Если MySQL установлен на том же сервере, что и Agora Invoicing, оставьте localhost',
+    'database_name_label' => 'Имя базы данных',
+    'mysql_port_label' => 'Номер порта MySQL',
+    'mysql_port_tooltip' => 'Номер порта, на котором работает ваш сервер MySQL. По умолчанию это 3306',
+    'username' => 'Имя пользователя',
+    'password_label' => 'Пароль',
+    'test_prerequisites_message' => 'Этот тест проверит предварительные требования для установки Agora Invoicing',
+    'previous' => 'Предыдущий',
+
+    'sign_up_as_admin' => 'Зарегистрироваться как администратор',
+    'first_name' => 'Имя',
+    'first_name_required' => 'Имя обязательно',
+    'last_name' => 'Фамилия',
+    'last_name_required' => 'Фамилия обязательна',
+    'username_info' => 'Имя пользователя может содержать только алфавитно-цифровые символы, пробелы, подчеркивания, дефисы, точки и символ @.',
+    'email' => 'Электронная почта',
+    'email_required' => 'Электронная почта пользователя обязательна',
+    'password_required' => 'Пароль обязателен',
+    'confirm_password' => 'Подтвердите пароль',
+    'confirm_password_required' => 'Подтверждение пароля обязательно',
+    'password_requirements' => 'Ваш пароль должен содержать:',
+    'password_requirements_list' => [
+        'От 8 до 16 символов',
+        'Заглавные буквы (A-Z)',
+        'Строчные буквы (a-z)',
+        'Цифры (0-9)',
+        'Специальные символы (~*!@$#%_+.?:,{ })',
     ],
 
-    /*
-     *
-     * Requirements page translations.
-     *
-     */
-    'requirements' => [
-        'title' => 'Необходимые модули',
-    ],
+// Системная информация
+    'system_information' => 'Системная информация',
+    'environment' => 'Окружение',
+    'environment_required' => 'Окружение обязательно',
+    'production' => 'Производственное',
+    'development' => 'Разработка',
+    'testing' => 'Тестирование',
+    'cache_driver' => 'Драйвер кэша',
+    'cache_driver_required' => 'Драйвер кэша обязателен',
+    'file' => 'Файл',
+    'redis' => 'Redis',
+    'password' => 'Пароль',
 
-    /*
-     *
-     * Permissions page translations.
-     *
-     */
-    'permissions' => [
-        'title' => 'Проверка прав на папках',
-    ],
+// Настройка Redis
+    'redis_setup' => 'Настройка Redis',
+    'redis_host' => 'Хост Redis',
+    'redis_port' => 'Порт Redis',
+    'redis_password' => 'Пароль Redis',
 
-    /*
-     *
-     * Environment page translations.
-     *
-     */
-    'environment' => [
-        'title' => 'Настройки окружения',
-        'save' => 'Сохранить .env',
-        'success' => 'Настройки успешно сохранены в файле .env',
-        'errors' => 'Произошла ошибка при сохранении файла .env, пожалуйста, сохраните его вручную',
-    ],
+// Кнопки
+    'continue' => 'Продолжить',
 
-    /*
-     *
-     * Final page translations.
-     *
-     */
-    'final' => [
-        'title' => 'Готово',
-        'finished' => 'Приложение успешно настроено.',
-        'exit' => 'Нажмите для выхода',
-    ],
+// Финальная настройка
+    'final_setup' => 'Ваше приложение Agora Invoicing готово!',
+    'installation_complete' => 'Отлично, вы успешно завершили установку.',
+
+// Узнать больше
+    'learn_more' => 'Узнать больше',
+    'knowledge_base' => 'База знаний',
+    'email_support' => 'Поддержка по электронной почте',
+
+// Следующий шаг
+    'next_step' => 'Следующий шаг',
+    'login_button' => 'Войти в Billing',
+
+    'pre_migration_success' => 'Предварительная миграция успешно протестирована',
+    'migrating_tables' => 'Миграция таблиц в базе данных',
+    'db_connection_error' => 'Ошибка обновления подключения к базе данных.',
+    'database_setup_success' => 'База данных успешно настроена.',
+    'env_file_created' => 'Файл конфигурации окружения успешно создан',
+    'pre_migration_test' => 'Запуск предварительного теста миграции',
+
+    'redis_host_required' => 'Хост Redis обязателен.',
+    'redis_password_required' => 'Пароль Redis обязателен.',
+    'redis_port_required' => 'Порт Redis обязателен.',
+    'password_regex' => 'Пароль должен содержать не менее 8 символов, одну заглавную букву, одну строчную букву, одну цифру и один специальный символ.',
+    'setup_completed' => 'Настройка успешно завершена!',
+
+    'database' => 'База данных',
+    'selected' => 'Выбрано',
+    'mysql_version_is' => 'Версия MySQL',
+    'database_empty' => 'База данных пуста',
+    'database_not_empty' => 'Установка Agora Invoicing требует пустую базу данных, ваша база данных уже содержит таблицы и данные.',
+    'mysql_version_required' => 'Рекомендуем обновиться до MySQL не ниже 5.6 или MariaDB 10.3!',
+    'database_connection_unsuccessful' => 'Не удалось подключиться к базе данных.',
+    'connected_as' => 'Подключено к базе данных как',
+    'failed_connection' => 'Не удалось подключиться к базе данных.',
+
 ];

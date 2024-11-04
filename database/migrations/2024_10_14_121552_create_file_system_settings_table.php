a2189acf69b1c413663e91c4f35b1400120ae833
sadhasivam-ladybirdweb <sadhasivam.sekar@ladybirdweb.com>
2024-11-04 10:13:48 +0530

    dc-1

--- database/migrations/2024_10_14_121552_create_file_system_settings_table.php
+++ database/migrations/2024_10_14_121552_create_file_system_settings_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->bigIncrements('id');
             $table->string('disk');
             $table->string('local_file_storage_path')->nullable();
-            $table->string('product_storage')->nullable();
             $table->string('s3_bucket')->nullable();
             $table->string('s3_region')->nullable();
             $table->string('s3_access_key')->nullable();

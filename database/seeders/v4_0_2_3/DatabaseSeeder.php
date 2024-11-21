8d6bc08254d4f6ee10c84160422b83aed4913573
sadhasivam <sadhasivam.sekar@ladybirdweb.com>
2024-11-21 09:50:32 +0530

    fix seeder

--- database/seeders/v4_0_2_3/DatabaseSeeder.php
+++ database/seeders/v4_0_2_3/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
     }
     public function fileSystemSeeder()
     {
-        FileSystemSettings::firstOrCreate([
+        FileSystemSettings::create([
             'disk' => 'system',
         ]);
     }

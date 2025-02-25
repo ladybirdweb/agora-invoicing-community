778f45e4e0f617549011242ffb57508e752288ca
sadhasivam-ladybirdweb <sadhasivam.sekar@ladybirdweb.com>
2025-02-25 16:42:32 +0530

    fix test cases

--- tests/Unit/Client/Stripe/SettingsControllerTest.php
+++ tests/Unit/Client/Stripe/SettingsControllerTest.php
@@ -114,15 +114,18 @@ class SettingsControllerTest extends DBTestCase
     // Test case for handling incorrect stripe token
     public function test_handlePayment_return_exception_incorrect_values()
     {
-        $requestData = ['stripeToken' => '12345678904567890'];
-        $expectedArguments = ['payment_method' => 'pm_card_visa', 'return_url' => 'https://example.com/return-url'];
-        $status = 'require_action';
-        $stripeClientConstructorMock = $this->setupStripeClientMock($expectedArguments, $status);
-        $requestMock = $this->setupRequestMock($requestData);
-        $this->SetAuthUser();
-        $controller = new SettingsController($stripeClientConstructorMock);
-        $response = $controller->handlePayment($requestMock, 50, 'INR', 'https://example.com/return-url', null);
-        $this->assertEquals('Invalid token id: 12345678904567890', $response->getMessage());
+        try{
+            $requestData = ['stripeToken' => '12345678904567890'];
+            $expectedArguments = ['payment_method' => 'pm_card_visa', 'return_url' => 'https://example.com/return-url'];
+            $status = 'require_action';
+            $stripeClientConstructorMock = $this->setupStripeClientMock($expectedArguments, $status);
+            $requestMock = $this->setupRequestMock($requestData);
+            $this->SetAuthUser();
+            $controller = new SettingsController($stripeClientConstructorMock);
+            $response = $controller->handlePayment($requestMock, 50, 'INR', 'https://example.com/return-url', null);
+        }catch (\Exception $exception) {
+            $this->assertEquals('Invalid token id: 12345678904567890', $exception->getMessage());
+        }
     }
 
     // Test case for handling autopay for 3ds with incomplete status
